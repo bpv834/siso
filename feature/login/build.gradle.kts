239f08a6cc7fe8d0726197c7ae3bcf9c75449a22
zzangse1 <zzangse1014@gmail.com>
2025-08-12 11:53:39 +0900

    build.gradle.kts login 의존성 추가

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -36,7 +36,15 @@ dependencies {
     implementation(project(":domain"))
     implementation(project(":core:navigation"))
     implementation(project(":core:ui"))
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 
     implementation(libs.dagger.hilt.android)
     implementation(libs.dagger.hilt.android.compiler)
