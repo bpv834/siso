4dc2ed4b28861c131413c6ce6089531030be2c54
jaeminKim <kg8480@gmail.com>
2025-08-14 01:00:49 +0900

    FirstLoginInfoScreen.kt FirstLoginInfoScreenViewModel.kt 폴더 이동

--- feature/login/src/main/java/com/likelion/login/FirstLoginInfoScreen/FirstLoginInfoScreenViewModel.kt
+++ feature/login/src/main/java/com/likelion/login/FirstLoginInfoScreen/FirstLoginInfoScreenViewModel.kt
@@ -1,18 +1,20 @@
-package com.likelion.login
+package com.likelion.login.FirstLoginInfoScreen
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
+@HiltViewModel
 class FirstLoginInfoScreenViewModel @Inject constructor(
     // usecase자리
 ): ViewModel() {
     private val _nameState = MutableStateFlow("")
     val nameState : String get() = _nameState.value
-    private val _fistContinueBoolean = MutableStateFlow(true)
-    val fistContinueBoolean : Boolean get() = _fistContinueBoolean.value
+    private val _firstContinueBoolean = MutableStateFlow(true)
+    val fistContinueBoolean : Boolean get() = _firstContinueBoolean.value
     private val _myRadioButtons = MutableStateFlow(
         mutableStateListOf(
             Pair(first = "남성", second = true),
@@ -31,5 +33,5 @@ class FirstLoginInfoScreenViewModel @Inject constructor(
         it + input
     }
 
-    fun fistContinueBooleanUpdate(input: Boolean) = _fistContinueBoolean.update { input }
+    fun fistContinueBooleanUpdate(input: Boolean) = _firstContinueBoolean.update { input }
 }
