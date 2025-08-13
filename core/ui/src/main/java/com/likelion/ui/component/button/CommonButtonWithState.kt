197124e2ece73efa4c7344c1fe3e4a79fc2d2431
ho <yungjoon2@naver.com>
2025-08-13 23:57:52 +0900

    style : 상태에 따라 분기되는 버튼 추가

---
+++ core/ui/src/main/java/com/likelion/ui/component/button/CommonButtonWithState.kt
@@ -0,0 +1,46 @@
+package com.likelion.ui.component.button
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.likelion.ui.theme.SisoColorTokens
+import com.likelion.ui.theme.SisoTypoTokens
+
+@Composable
+fun CommonButtonWithState(
+    text: String,
+    onClick: () -> Unit,
+    isActive: Boolean, // 버튼 활성화 상태
+
+) {
+    // isActive 상태에 따라 색상과 보더를 동적으로 결정
+    val containerColor = if (isActive) SisoColorTokens.Gold40 else SisoColorTokens.GrayScale30
+    val contentColor = if (isActive) SisoColorTokens.GrayScale90 else SisoColorTokens.GrayScale50
+    val borderColor = if (isActive) SisoColorTokens.Gold80 else SisoColorTokens.GrayScale40
+
+    OutlinedButton(
+        onClick = onClick,
+        enabled = isActive, // isActive 상태에 따라 버튼 활성화/비활성화
+        border = BorderStroke(1.dp, borderColor),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = containerColor,
+            contentColor = contentColor,
+            disabledContainerColor = SisoColorTokens.GrayScale30, // 비활성화 상태 색상
+            disabledContentColor = SisoColorTokens.GrayScale50 // 비활성화 상태 텍스트 색상
+        ),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = text,
+            style = SisoTypoTokens.Button1,
+            color = contentColor, // 여기서는 contentColor를 사용하여 텍스트 색상 결정
+            modifier = Modifier.padding(vertical = (15.5).dp)
+        )
+    }
+}
