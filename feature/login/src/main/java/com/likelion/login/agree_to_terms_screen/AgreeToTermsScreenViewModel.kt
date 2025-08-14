694ce42b2588e15a6467b71f8e1707c6da144993
jaeminKim <kg8480@gmail.com>
2025-08-14 21:46:27 +0900

    AgreeToTermsScreen.kt 뷰모델 수정

--- feature/login/src/main/java/com/likelion/login/agree_to_terms_screen/AgreeToTermsScreenViewModel.kt
+++ feature/login/src/main/java/com/likelion/login/agree_to_terms_screen/AgreeToTermsScreenViewModel.kt
@@ -1,10 +1,12 @@
 package com.likelion.login.agree_to_terms_screen
 
+import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.forEach
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
@@ -13,25 +15,10 @@ class AgreeToTermsScreenViewModel @Inject constructor(
     // usecase자리
 ): ViewModel(), AgreeToTermsScreenViewModelType {
     private val _agreeContinueBoolean = MutableStateFlow(false)
-    override val agreeContinueBoolean : Boolean get() = _agreeContinueBoolean.value
-    private val _agreesBoolean = MutableStateFlow(
-        mutableListOf(
-            Pair("(필수) 이용약관 동의",false),
-            Pair("(선택) 마케팅 정보 수신",false),
-        )
-    )
-    override val agreesBoolean : MutableList<Pair<String, Boolean>> get() = _agreesBoolean.value
+    override val agreeContinueBoolean : StateFlow<Boolean> get() = _agreeContinueBoolean.asStateFlow()
 
-    override fun agreeContinueBooleanUpdate(index:Int) = _agreeContinueBoolean.update {
-        val term = agreesBoolean[index]
-        agreesBoolean[index] = term.copy(
-            term.first,!term.second
-        )
-        var agree = true
-        agreesBoolean.forEach {
-            agree = agree&&it.second
-        }
-        agree
+    override fun agreeContinueBooleanUpdate(agreeStates: Boolean) = _agreeContinueBoolean.update {
+        agreeStates
     }
 
 
