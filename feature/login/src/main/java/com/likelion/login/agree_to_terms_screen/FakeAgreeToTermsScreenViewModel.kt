d6c9cf010bb898427d619fb19c2ebfcdbdaec932
jaeminKim <kg8480@gmail.com>
2025-08-14 21:47:47 +0900

    AgreeToTermsScreen.kt fake 뷰모델 수정

--- feature/login/src/main/java/com/likelion/login/agree_to_terms_screen/FakeAgreeToTermsScreenViewModel.kt
+++ feature/login/src/main/java/com/likelion/login/agree_to_terms_screen/FakeAgreeToTermsScreenViewModel.kt
@@ -1,32 +1,20 @@
 package com.likelion.login.agree_to_terms_screen
 
+import androidx.compose.runtime.mutableStateListOf
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class FakeAgreeToTermsScreenViewModel (
     // usecase자리
 ):AgreeToTermsScreenViewModelType {
     private val _agreeContinueBoolean = MutableStateFlow(false)
-    override val agreeContinueBoolean: Boolean get() = _agreeContinueBoolean.value
-    private val _agreesBoolean = MutableStateFlow(
-        mutableListOf(
-            Pair("(필수) 이용약관 동의", false),
-            Pair("(선택) 마케팅 정보 수신", false),
-        )
-    )
-    override val agreesBoolean: MutableList<Pair<String, Boolean>> get() = _agreesBoolean.value
+    override val agreeContinueBoolean: StateFlow<Boolean> get() = _agreeContinueBoolean.asStateFlow()
 
-    override fun agreeContinueBooleanUpdate(index: Int) = _agreeContinueBoolean.update {
-        val term = agreesBoolean[index]
-        agreesBoolean[index] = term.copy(
-            term.first, !term.second
-        )
-        var agree = true
-        agreesBoolean.forEach {
-            agree = agree && it.second
-        }
-        agree
+    override fun agreeContinueBooleanUpdate(agreeStates: Boolean) = _agreeContinueBoolean.update {
+        agreeStates
     }
+
+
 }
-
-
