91f67271a97bfe8d2d66e29074fcfa2d35d1b578
jaeminKim <kg8480@gmail.com>
2025-08-14 21:45:56 +0900

    AgreeToTermsScreen.kt 화면 뷰 기능 수정

--- feature/login/src/main/java/com/likelion/login/agree_to_terms_screen/AgreeToTermsScreen.kt
+++ feature/login/src/main/java/com/likelion/login/agree_to_terms_screen/AgreeToTermsScreen.kt
@@ -1,10 +1,12 @@
 package com.likelion.login.agree_to_terms_screen
 
-import android.R.attr.contentDescription
 import android.R.attr.onClick
+import android.R.attr.text
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,68 +17,93 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
 import com.likelion.login.first_loginInfo_screen.FontText
 import com.likelion.ui.R
 import com.likelion.ui.theme.SisoColorTokens
 import com.likelion.ui.theme.SisoTypoTokens
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlin.collections.forEachIndexed
 
-@SuppressLint("StateFlowValueCalledInComposition")
+@SuppressLint("StateFlowValueCalledInComposition", "UnrememberedMutableState")
 @Composable
 fun AgreeToTermsScreen(
-    viewModel: AgreeToTermsScreenViewModel = AgreeToTermsScreenViewModel()
+    viewModel: AgreeToTermsScreenViewModelType
 ) {
     val sideDp = 16.dp
     val scrollState = rememberScrollState()
     val introductionFontStyle = SisoTypoTokens.Body1
+    val agreeList = listOf(
+            1L to "(필수) 이용약관 동의",
+            2L to "(선택) 마케팅 정보 수신",
+    )
+    val agreeContinueBoolean by viewModel.agreeContinueBoolean.collectAsStateWithLifecycle()
+
     Column {
         Column(
-            modifier = Modifier.padding(start = sideDp, end = sideDp)
+            modifier = Modifier
                 .verticalScroll(scrollState),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = AbsoluteAlignment.Left
         ) {
             Spacer(Modifier.padding(50.dp))
+            Column(
+                modifier = Modifier
+                    .padding(start = sideDp, end = sideDp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = AbsoluteAlignment.Left
+            ) {
+                FontText(
+                    text = "시팅에 어서오세요",
+                    style = introductionFontStyle,
+                    textColor = SisoColorTokens.GrayScale90,
+                    padding = 0.dp,
+                )
+                FontText(
+                    text = "새로운 인연을 만나기전에",
+                    style = introductionFontStyle,
+                    textColor = SisoColorTokens.GrayScale90,
+                    padding = 0.dp,
+                )
+                FontText(
+                    text = "동의가 필요해요",
+                    style = introductionFontStyle,
+                    textColor = SisoColorTokens.GrayScale90,
+                    padding = 0.dp,
+                )
+            }
 
-            FontText(
-                text = "시팅에 어서오세요",
-                style = introductionFontStyle,
-                textColor = SisoColorTokens.GrayScale90,
-                padding = 0.dp,
-            )
-            FontText(
-                text = "새로운 인연을 만나기전에",
-                style = introductionFontStyle,
-                textColor = SisoColorTokens.GrayScale90,
-                padding = 0.dp,
-            )
-            FontText(
-                text = "동의가 필요해요",
-                style = introductionFontStyle,
-                textColor = SisoColorTokens.GrayScale90,
-                padding = 0.dp,
-            )
 
             Spacer(Modifier.padding(35.dp))
-            AgreeRepeatRadioButton(
-                radio = viewModel.agreesBoolean,
-                onClick = {index->
-                    viewModel.agreeContinueBooleanUpdate(index = index)
-                },
-            )
+            Column(
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+            ) {
+                AgreeRepeatRadioButton(
+                    termsList = agreeList,
+                    onClick = {continueBoolean->
+                        viewModel.agreeContinueBooleanUpdate(continueBoolean)
+                        Log.d("radioRemember", "[agreeContinueBoolean]")
+                        Log.d("radioRemember", viewModel.agreeContinueBoolean.toString())
+                    },
+                )
+            }
+
 
 
 
@@ -84,7 +111,8 @@ fun AgreeToTermsScreen(
 
         }
         Button(
-            modifier = Modifier.padding(start = sideDp, end = sideDp)
+            modifier = Modifier
+                .padding(start = sideDp, end = sideDp)
                 .fillMaxWidth()
                 .height(65.dp),
             colors = ButtonDefaults.buttonColors(
@@ -94,12 +122,12 @@ fun AgreeToTermsScreen(
             onClick = {
 
             },
-            enabled = viewModel.agreeContinueBoolean
+            enabled = agreeContinueBoolean
         ) {
             Text(
-                text = "계속하기",
+                text = "모두 동의",
                 style = SisoTypoTokens.Button1,
-                color = if(viewModel.agreeContinueBoolean == true)SisoColorTokens.GrayScale90
+                color = if(agreeContinueBoolean == true)SisoColorTokens.GrayScale90
                 else SisoColorTokens.GrayScale60,
                 fontSize = 22.sp
             )
@@ -113,46 +141,63 @@ fun AgreeToTermsScreen(
 @SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 fun AgreeRepeatRadioButton(
-    radio: MutableList<Pair<String, Boolean>>,
-    onClick:(Int)->Unit = {}
+    termsList: List<Pair<Long, String>>,
+    onClick:(Boolean)->Unit = {}
 ){
-    radio.forEachIndexed { index, info ->
+    val checkIdList  = remember {
+        mutableStateListOf<Long>()
+    }
+
+    termsList.forEachIndexed { idx, (id, text) ->
         Row(
-            modifier = Modifier.clickable{
-                onClick(index)
-            }.padding(top = 22.dp, bottom = 22.dp),
+            modifier = Modifier
+                .clickable {
+                    if (checkIdList.contains(id)) {
+                        checkIdList.remove(id)
+                    } else {
+                        checkIdList.add(id)
+                    }
+
+                    onClick(checkIdList.size == termsList.size)
+                    Log.d("radioRemember", "[onClick]")
+                    Log.d("radioRemember", "[checkIdList]")
+                    Log.d("radioRemember", "${checkIdList.size == termsList.size}")
+                    Log.d("radioRemember", checkIdList.toString())
+                }
+                .padding(top = 22.dp, bottom = 22.dp, start = 8.dp, end = 8.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
             FontText(
                 fillMaxFloat = 0.9F,
-                text = info.first, style = SisoTypoTokens.Body4, padding = 0.dp,
+                text = text, style = SisoTypoTokens.Body4, padding = 0.dp,
                 textColor = SisoColorTokens.GrayScale90,
             )
+
             AsyncImage(
                     modifier = Modifier.clickable(
                         onClick = {
-                            onClick(index)
+                            if (checkIdList.contains(id)) {
+                                checkIdList.remove(id)
+                            } else {
+                                checkIdList.add(id)
+                            }
+
+                            onClick(checkIdList.size == termsList.size)
+                            Log.d("radioRemember", "[onClick]")
+                            Log.d("radioRemember", "[checkIdList]")
+                            Log.d("radioRemember", checkIdList.toString())
                         }
                     ),
-                    model = if (info.second) R.drawable.select
+                    model =  if (checkIdList.contains(id))R.drawable.select
                     else R.drawable.unselect,
                     contentDescription = ""
                 )
-/*            RadioButton(
-                selected = info.second,
-                colors = RadioButtonDefaults.colors(
-                    selectedColor = SisoColorTokens.GrayScale90,
-                    unselectedColor = SisoColorTokens.GrayScale30
-                ),
-                onClick = {
-                    radios[index] = info.copy(info.first,!info.second)
-                    onClick()
-                }
 
-            )*/
         }
     }
+    Log.d("radioRemember", "[radioRemember]")
+    Log.d("radioRemember", checkIdList.toString())
 }
 
 @Composable
@@ -161,7 +206,7 @@ fun AgreeToTermsScreenPreview(){
     Surface(
         color = SisoColorTokens.White
     ){
-        AgreeToTermsScreen()
+        AgreeToTermsScreen(FakeAgreeToTermsScreenViewModel())
     }
 
 }
