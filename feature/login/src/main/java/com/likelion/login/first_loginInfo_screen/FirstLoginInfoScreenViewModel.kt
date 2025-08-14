353d5cab98d800631d1afc0e49c0fe460e4bd25d
jaeminKimPea <kg8480@gmail.com>
2025-08-14 16:30:31 +0900

    FirstLoginInfoScreen.kt 의 뷰모델 타입

--- feature/login/src/main/java/com/likelion/login/first_loginInfo_screen/FirstLoginInfoScreenViewModel.kt
+++ feature/login/src/main/java/com/likelion/login/first_loginInfo_screen/FirstLoginInfoScreenViewModel.kt
@@ -9,29 +9,36 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FirstLoginInfoScreenViewModel @Inject constructor(
+
     // usecase자리
-): ViewModel() {
+): ViewModel(), FirstLoginInfoScreenViewModelType {
     private val _nameState = MutableStateFlow("")
-    val nameState : String get() = _nameState.value
+    override val nameState : String get() = _nameState.value
+    private val _ageState = MutableStateFlow("")
+    override val ageState: String get() = _ageState.value
     private val _firstContinueBoolean = MutableStateFlow(true)
-    val fistContinueBoolean : Boolean get() = _firstContinueBoolean.value
+    override val fistContinueBoolean : Boolean get() = _firstContinueBoolean.value
     private val _myRadioButtons = MutableStateFlow(
         mutableStateListOf(
             Pair(first = "남성", second = true),
             Pair(first = "여성", second = false),
         )
     )
-    val myRadioButtons : MutableList<Pair<String, Boolean>> get() = _myRadioButtons.value
+    override val myRadioButtons : MutableList<Pair<String, Boolean>> get() = _myRadioButtons.value
     private val _pairRadioButtons = MutableStateFlow(
         mutableStateListOf(
             Pair(first = "동성", second = true),
             Pair(first = "이성", second = false),
         )
     )
-    val pairRadioButtons : MutableList<Pair<String, Boolean>> get() = _pairRadioButtons.value
-    fun nameUpdate(input: String) = _nameState.update {
+    override val pairRadioButtons : MutableList<Pair<String, Boolean>> get() = _pairRadioButtons.value
+    override fun nameUpdate(input: String) = _nameState.update {
         it + input
     }
 
-    fun fistContinueBooleanUpdate(input: Boolean) = _firstContinueBoolean.update { input }
+    override fun ageUpdate(input: String) = _nameState.update {
+        it + input
+    }
+
+    override fun fistContinueBooleanUpdate(input: Boolean) = _firstContinueBoolean.update { input }
 }
