c1f97f350a2c96f023cfad7321957b32a16949e1
jaeminKim <kg8480@gmail.com>
2025-08-14 21:46:40 +0900

    AgreeToTermsScreen.kt 뷰모델 타입 수정

--- feature/login/src/main/java/com/likelion/login/agree_to_terms_screen/AgreeToTermsScreenViewModelType.kt
+++ feature/login/src/main/java/com/likelion/login/agree_to_terms_screen/AgreeToTermsScreenViewModelType.kt
@@ -1,10 +1,10 @@
 package com.likelion.login.agree_to_terms_screen
 
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 
 interface AgreeToTermsScreenViewModelType {
-    val agreeContinueBoolean : Boolean
-    val agreesBoolean : MutableList<Pair<String, Boolean>>
-    fun agreeContinueBooleanUpdate(index:Int)
+    val agreeContinueBoolean : StateFlow<Boolean>
+    fun agreeContinueBooleanUpdate(agreeStates: Boolean)
 }
