7fe6620a8073a109ac215f38c6017187250a2aa1
jaeminKim <kg8480@gmail.com>
2025-08-16 13:53:30 +0900

    화면 크기 수정

--- feature/login/src/main/java/com/likelion/login/login_start_screen/LoginStartScreen.kt
+++ feature/login/src/main/java/com/likelion/login/login_start_screen/LoginStartScreen.kt
@@ -1,8 +1,10 @@
 package com.likelion.login.login_start_screen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,6 +18,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.AbsoluteAlignment
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,7 +36,9 @@ fun LoginStartScreen(
     val scrollState = rememberScrollState()
     val introductionFontStyle = SisoTypoTokens.Body1
     val textBoxSize = 99
-    Column {
+    Box(
+        modifier = Modifier.fillMaxHeight()
+    ) {
         Column(
             modifier = Modifier
                 .verticalScroll(scrollState),
@@ -72,29 +77,33 @@ fun LoginStartScreen(
             Spacer(Modifier.size(size = 59.1.dp))
 
         }
-
-        Button(
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
-                .fillMaxWidth()
-                .height(65.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = SisoColorTokens.Orange30,
-                disabledContainerColor = SisoColorTokens.GrayScale50
-            ),
-            onClick = {
-                viewModel.fistContinueBooleanUpdate(true)
-            },
-            enabled = viewModel.fistContinueBoolean
+        Column(
+            modifier = Modifier.align(Alignment.BottomCenter)
         ) {
-            Text(
-                text = "계속하기",
-                style = SisoTypoTokens.Button1,
-                color = if(viewModel.fistContinueBoolean == true)SisoColorTokens.GrayScale90
-                else SisoColorTokens.GrayScale50,
-                fontSize = 22.sp
-            )
+            Button(
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+                    .fillMaxWidth()
+                    .height(65.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = SisoColorTokens.Orange30,
+                    disabledContainerColor = SisoColorTokens.GrayScale50
+                ),
+                onClick = {
+                    viewModel.fistContinueBooleanUpdate(true)
+                },
+                enabled = viewModel.fistContinueBoolean
+            ) {
+                Text(
+                    text = "계속하기",
+                    style = SisoTypoTokens.Button1,
+                    color = if(viewModel.fistContinueBoolean == true)SisoColorTokens.GrayScale90
+                    else SisoColorTokens.GrayScale50,
+                    fontSize = 22.sp
+                )
+            }
+            Spacer(Modifier.size(size = 56.dp))
         }
-        Spacer(Modifier.size(size = 56.dp))
+
     }
 
 
