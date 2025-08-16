98cfd5e49951f9a9d86395272e0d1de6b47cc4a7
jaeminKim <kg8480@gmail.com>
2025-08-16 13:53:24 +0900

    화면 크기 수정

--- feature/login/src/main/java/com/likelion/login/first_loginInfo_screen/FirstLoginInfoScreen.kt
+++ feature/login/src/main/java/com/likelion/login/first_loginInfo_screen/FirstLoginInfoScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -62,6 +63,7 @@ fun FirstLoginInfoScreen(
     Column(
         modifier = Modifier
             .padding(start = sideDp, end = sideDp)
+            .fillMaxHeight()
             .verticalScroll(scrollState),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = AbsoluteAlignment.Left
