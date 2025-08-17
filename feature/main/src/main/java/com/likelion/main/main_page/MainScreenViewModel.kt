40b664b37fc27a758efbe7666b17bb8391307ab2
bpv834 <yungjoon2@naver.com>
2025-08-17 14:44:49 +0900

    feat :  메인 화면 구성

--- feature/main/src/main/java/com/likelion/main/main_page/MainScreenViewModel.kt
+++ feature/main/src/main/java/com/likelion/main/main_page/MainScreenViewModel.kt
@@ -1,7 +1,31 @@
 package com.likelion.main.main_page
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.likelion.domain.model.UsersModel
+import com.likelion.domain.usecase.GetAllUsersUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MainScreenViewModel @Inject constructor() : ViewModel(), MainScreenViewModelType {
+@HiltViewModel
+class MainScreenViewModel @Inject constructor(
+    val getAllUsersUseCase: GetAllUsersUseCase
+) : ViewModel(), MainScreenViewModelType {
+    val _userList = MutableStateFlow<List<UsersModel>>(emptyList())
+    override val userList : StateFlow<List<UsersModel>> = _userList.asStateFlow()
+
+    init {
+        getUserList()
+    }
+
+   @Override
+   override fun getUserList(){
+        viewModelScope.launch {
+            _userList.value = getAllUsersUseCase.execute()
+        }
+    }
 }
--- feature/main/src/main/java/com/likelion/main/main_page/MainScreenViewModelType.kt
+++ feature/main/src/main/java/com/likelion/main/main_page/MainScreenViewModelType.kt
@@ -1,4 +1,10 @@
 package com.likelion.main.main_page
 
+import com.likelion.domain.model.UsersModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
 interface MainScreenViewModelType {
+    val userList: StateFlow<List<UsersModel>>
+    fun getUserList()
 }
