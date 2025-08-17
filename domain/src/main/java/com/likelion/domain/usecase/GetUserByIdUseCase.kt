1976b4ffafc838266b027d8dd62357b067551d92
bpv834 <yungjoon2@naver.com>
2025-08-17 14:46:13 +0900

    feat :  model 단 유저정보관련 데이터 usecase 테스트용 구현

--- domain/src/main/java/com/likelion/domain/usecase/GetUserByIdUseCase.kt
+++ domain/src/main/java/com/likelion/domain/usecase/GetUserByIdUseCase.kt
@@ -3,11 +3,11 @@ package com.likelion.domain.usecase
 import com.likelion.domain.model.UsersModel
 import com.likelion.domain.repository.UsersRepository
 
-class GetAllUsersUseCase(
+class GetUserByIdUseCase(
     private val usersRepository: UsersRepository
 ) {
     // Usecase는 Repository가 반환하는 UsersModel 리스트를 받습니다.
-    suspend fun execute(): List<UsersModel> {
-        return usersRepository.getAllUsers()
+    suspend fun execute(id: Long): UsersModel {
+        return usersRepository.getUserById(id=id)
     }
 }
