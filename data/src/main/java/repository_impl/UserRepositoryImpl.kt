5318d9c55ff748d4f4afd14c0253a35a626c3074
bpv834 <yungjoon2@naver.com>
2025-08-17 14:45:23 +0900

    feat :  data단 repo 구현(fake)

--- data/src/main/java/repository_impl/UserRepositoryImpl.kt
+++ data/src/main/java/repository_impl/UserRepositoryImpl.kt
@@ -53,4 +53,24 @@ class UserRepositoryImpl(
             }
         }
     }
+
+    override suspend fun getUserById(id: Long): UsersModel {
+        val fakeUser = UsersModel(
+            id = 4L,
+            isOnline = true,
+            userImages = listOf(
+                "http://www.civicnews.com/news/photo/201811/19147_26513_953.png",
+                "https://cdn.ntoday.co.kr/news/photo/202101/77115_50584_1928.jpg"
+            ),
+            location = "America",
+            nickname = "코딩러",
+            age = 65,
+            voiceUrl = "https://example.com/voice1.mp3",
+            interests = listOf("풋볼", "영화", "음악"),
+            introduce = "안녕하세요. 코딩을 좋아하는 개발자입니다 / 안녕하세요. 코딩을 좋아하는 개발자입니다 / 안녕하세요. 코딩을 좋아하는 개발자입니다 /" +
+                    " 안녕하세요. 코딩을 좋아하는 개발자입니다 /" +
+                    " 안녕하세요. 코딩을 좋아하는 개발자입니다."
+        )
+        return fakeUser
+    }
 }
