09e1aff3ab230830f4012d3ee17c2db54b73a737
jaeminKim <kg8480@gmail.com>
2025-08-17 17:15:09 +0900

    마이페이지 8/17 본으로 수정

--- feature/mypage/src/main/java/com/lion/mypage/MyPageScreen.kt
+++ feature/mypage/src/main/java/com/lion/mypage/MyPageScreen.kt
@@ -1,39 +1,22 @@
 package com.lion.mypage
 
-import android.R.attr.contentDescription
-import android.R.attr.end
-import android.R.attr.text
-import android.R.attr.top
 import android.util.Log.d
-import android.widget.Button
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -43,26 +26,26 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.asComposePath
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.rotationMatrix
-import androidx.graphics.shapes.CornerRounding
-import androidx.graphics.shapes.Morph
-import androidx.graphics.shapes.RoundedPolygon
-import androidx.graphics.shapes.toPath
 import coil3.compose.AsyncImage
 import com.likelion.ui.theme.SisoColorTokens
 import com.likelion.ui.theme.SisoTheme
 import com.likelion.ui.theme.SisoTypoTokens
+import com.lion.mypage.setting_screen.SettingText
 
 @Composable
 fun MyPageScreen()  {
+
+
+    val profileOption = listOf(
+        "통화 기록" to {},
+        "차단 / 신고한 인연" to {},
+        "매칭 필터 설정" to {},
+    )
+
     Column(
         modifier = Modifier.padding(start = 16.dp, end = 16.dp)
             .fillMaxHeight()
@@ -71,29 +54,38 @@ fun MyPageScreen()  {
         Row {
             ProfileCircle(
                 padding = 8.dp,
-                processFloat = .88F
+                processFloat = .36F
             )
             Column(
                 modifier = Modifier.offset(x = ((-2).dp))
                     .padding(end = 10.dp)
             ) {
-                Spacer(modifier = Modifier.size(size = 22.dp))
+                Spacer(modifier = Modifier.size(size = 6.dp))
                 Text(
                     modifier = Modifier.padding(start = 18.dp),
                     text = "따뜻한 봄날",
                     style = SisoTypoTokens.Title2,
                     color = SisoColorTokens.GrayScale90,
                 )
+                Spacer(modifier = Modifier.size(size = 2.dp))
                 Text(
                     modifier = Modifier.padding(start = 18.dp),
                     text = "56세",
                     style = SisoTypoTokens.Title3,
                     color = SisoColorTokens.GrayScale70,
                 )
-                Spacer(modifier = Modifier.size(size = 16.dp))
+                Spacer(modifier = Modifier.size(size = 10.dp))
+                Text(
+                    modifier = Modifier.padding(start = 18.dp),
+                    text = "서울 중구",
+                    style = SisoTypoTokens.Label1,
+                    color = SisoColorTokens.GrayScale90,
+                )
+                Spacer(modifier = Modifier.size(size = 11.dp))
                 AsyncImage(
                     modifier = Modifier
-                        .size(width = 194.dp, height = 48.dp)
+                        .size(width = 190.dp, height = 49.dp)
+                        .offset(x = (-6).dp)
                         .clickable{
                             d("test","click")
                         },
@@ -142,21 +134,36 @@ fun MyPageScreen()  {
                 }*/
             }
         }
-        Spacer(modifier = Modifier.size(size = 37.dp))
-        Row(
-            modifier = Modifier.horizontalScroll(rememberScrollState())
+        Spacer(modifier = Modifier.size(size = 27.dp))
 
-        ) {
-            Box(
+            Box (
                 modifier = Modifier
-                    .fillMaxWidth().height(100.dp)
-                    .border(width = 1.dp, color = SisoColorTokens.GrayScale30)
-            ) {
-
+                    .height(72.dp)
+                    .background(SisoColorTokens.GrayScale20, RoundedCornerShape(24.dp))
+            ){
+                Text(
+                    modifier = Modifier.align(Alignment.Center)
+                        .padding(start = 16.dp, end = 16.dp, top = 12.dp, bottom = 12.dp),
+                    text = "안녕하세요? 식물과 자연을 좋아하는 닉네임입니다.",
+                    style = SisoTypoTokens.Body2,
+                    color = SisoColorTokens.GrayScale90,
+                )
             }
+
+
+        Spacer(modifier = Modifier.size(size = 19.dp))
+
+        Text(
+            text = "계정정보",
+            style = SisoTypoTokens.SubTitle1,
+            color = SisoColorTokens.GrayScale90,
+        )
+
+        Spacer(modifier = Modifier.size(size = 12.dp))
+
+        profileOption.forEachIndexed { idx, (text,option)->
+            ProfileText(input = text,option = option)
         }
-
-
 
     }
 }
@@ -171,41 +178,62 @@ fun ProfileCircle(
     var progress by remember { mutableFloatStateOf(processFloat) }
     Box(
         modifier = Modifier.padding(start = padding)
+            .size(128.dp, 153.dp)
     ) { // 박스 우선 순위 아래 부터 그려짐
         // 2 이미지가 다음으로 그러졈
         AsyncImage(
-            modifier = Modifier.offset(x = 3.dp,2.dp)
-                .size(120.dp),
+            modifier = Modifier.size(120.dp, 120.dp),
             model = com.likelion.ui.R.drawable.example_profile,
             contentDescription = ""
         )
         // 1 서클이 먼저 그려짐
         CircularProgressIndicator(
             progress = { progress },
-            modifier = Modifier.size(124.dp),
+            modifier = Modifier.size(128.dp)
+                .offset(x = (-4).dp, y = (-4).dp),
             color = SisoColorTokens.Gold40,
-            strokeWidth = 4.dp,
+            strokeWidth = 8.dp,
             trackColor = SisoColorTokens.White.copy(alpha = 0.0F),
             strokeCap = StrokeCap.Round,
         )
 
         Box(
             modifier = Modifier.align(Alignment.BottomCenter)
-                .offset(y = (27.1).dp)
-                .size(92.dp, 39.dp)
+                .size(96.dp, 43.dp)
                 .border(2.dp, SisoColorTokens.Gold40, RoundedCornerShape(99.dp))
-                .background(SisoColorTokens.White, RoundedCornerShape(99.dp))
+                .background(SisoColorTokens.GrayScale5, RoundedCornerShape(99.dp))
         ){
-            Text(text = "36% 완성",
+            Text(
+                text = "36% 완성",
                 style = SisoTypoTokens.SubTitle1,
-                modifier = Modifier.align(Alignment.Center),
+                modifier = Modifier.align(Alignment.Center)
+                    .padding(start = 12.dp, end = 12.dp, top = 8.dp, bottom = 8.dp),
             )
         }
 
-
-
-
     }
+
+}
+
+@Composable
+fun ProfileText(
+    input: String,
+    option: () -> Unit
+){
+    Text(
+        modifier = Modifier.fillMaxWidth()
+            .padding(start = 16.dp, top = 16.dp, bottom = 16.dp)
+            .clickable{
+                option()
+            },
+        text = input,
+        style = SisoTypoTokens.Body2,
+        color = SisoColorTokens.Black,
+    )
+    HorizontalDivider(
+        thickness = 1.dp,
+        color = SisoColorTokens.GrayScale30
+    )
 
 }
 
