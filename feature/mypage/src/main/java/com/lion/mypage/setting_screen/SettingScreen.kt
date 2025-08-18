1a97045b33eab017a091e6ce01fd2f3703852293
jaeminKimPea <kg8480@gmail.com>
2025-08-18 09:47:53 +0900

    SettingScreen.kt 계정정보 클릭 범위 수정

--- feature/mypage/src/main/java/com/lion/mypage/setting_screen/SettingScreen.kt
+++ feature/mypage/src/main/java/com/lion/mypage/setting_screen/SettingScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -61,6 +63,7 @@ fun SettingText(
 ){
     Column (
         modifier = Modifier
+            .clip(RoundedCornerShape(3.dp))
             .clickable{
                 option()
             }
