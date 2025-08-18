ffbe82e6f402de0f8dbc4f781a6f59de72c977ca
bpv834 <yungjoon2@naver.com>
2025-08-18 21:16:09 +0900

    Feat : App.kt -> Timber 위한 앱단 초기화 gradle (core/util) : timber api 등록 gradle (app) timber : 사용위한 core/util 의존성 주입 gradle (core/ui) : ui 에서 timber 사용 위한 의존성 주입

--- app/src/main/java/com/likelion/siso/App.kt
+++ app/src/main/java/com/likelion/siso/App.kt
@@ -3,11 +3,18 @@ package com.likelion.siso
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            // 디버그 모드일 때만 로그 찍도록
+            Timber.plant(Timber.DebugTree())
+        }
+
         KakaoSdk.init(this, BuildConfig.NATIVE_APP_KEY)
     }
 }
