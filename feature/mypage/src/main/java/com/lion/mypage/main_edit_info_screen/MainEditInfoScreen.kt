d07ed765c20fd82c36d4890e3bbb5a6bce877330
jaeminKim <kg8480@gmail.com>
2025-08-19 18:06:19 +0900

    MainEditInfoScreen.kt 초기 생성

--- feature/mypage/src/main/java/com/lion/mypage/main_edit_info_screen/MainEditInfoScreen.kt
+++ feature/mypage/src/main/java/com/lion/mypage/main_edit_info_screen/MainEditInfoScreen.kt
@@ -1,9 +1,13 @@
 package com.lion.mypage.main_edit_info_screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 
 @Composable
 fun MainEditInfoScreen(
     viewModel: MainEditInfoScreenViewModelType
 ) {
+    Column {
+
+    }
 }
