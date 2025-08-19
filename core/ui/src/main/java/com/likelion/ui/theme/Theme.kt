8e75b6449022ff9893db204b0058ae96325b21ea
ho <yungjoon2@naver.com>
2025-08-19 12:28:40 +0900

    Feat : 테마 suface 변경

--- core/ui/src/main/java/com/likelion/ui/theme/Theme.kt
+++ core/ui/src/main/java/com/likelion/ui/theme/Theme.kt
@@ -8,7 +8,8 @@ import androidx.compose.runtime.Composable
 fun SisoTheme(content: @Composable () -> Unit) {
     MaterialTheme(
         colorScheme = lightColorScheme(
-            background = SisoColorTokens.White
+            background = SisoColorTokens.White,
+            surface = SisoColorTokens.White
         ),
     ) {
         content()
