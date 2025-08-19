f0c7531f920b8c05bfd9c58d489011f7b12781ab
jaeminKim <kg8480@gmail.com>
2025-08-19 18:16:00 +0900

    LocationEditInfoScreen.kt 초기 생성

---
+++ feature/mypage/src/main/java/com/lion/mypage/location_edit_info_screen/LocationEditInfoScreenViewModel.kt
@@ -0,0 +1,11 @@
+package com.lion.mypage.location_edit_info_screen
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class LocationEditInfoScreenViewModel(
+    //usecase자리
+): ViewModel(), LocationEditInfoScreenViewModelType {
+
+}
