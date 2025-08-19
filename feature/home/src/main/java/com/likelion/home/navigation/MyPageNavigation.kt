abb11210339bd284245316f358e88c354fec0e49
zzangse1 <zzangse1014@gmail.com>
2025-08-19 09:40:06 +0900

    Feat: 내 정보 네비게이션 추가

---
+++ feature/home/src/main/java/com/likelion/home/navigation/MyPageNavigation.kt
@@ -0,0 +1,23 @@
+package com.likelion.home.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.likelion.home.chat.ChatRoute
+import com.likelion.home.home.HomeRoute
+import com.likelion.home.mypage.MyPageRoute
+import com.likelion.navigation.NavigationRoute
+
+fun NavController.navigateToMyPage(navOptions: NavOptions? = null) =
+    navigate(NavigationRoute.MyPageScreen.route, navOptions)
+
+fun NavGraphBuilder.myPageNavigation(action: () -> Unit) {
+    composable(
+        route = NavigationRoute.MyPageScreen.route
+    ) {
+        MyPageRoute(
+            actionSnackbar = action
+        )
+    }
+}
