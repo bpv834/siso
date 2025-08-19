662ee00ec16264c6c15dde10a5ab74331eabf0d5
zzangse1 <zzangse1014@gmail.com>
2025-08-19 09:37:33 +0900

    Feat: 채팅 네비게이션 설정

---
+++ feature/home/src/main/java/com/likelion/home/navigation/ChatNavigation.kt
@@ -0,0 +1,22 @@
+package com.likelion.home.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.likelion.home.chat.ChatRoute
+import com.likelion.home.home.HomeRoute
+import com.likelion.navigation.NavigationRoute
+
+fun NavController.navigateToChat(navOptions: NavOptions? = null) =
+    navigate(NavigationRoute.ChatScreen.route, navOptions)
+
+fun NavGraphBuilder.chatNavigation(action: () -> Unit) {
+    composable(
+        route = NavigationRoute.ChatScreen.route
+    ) {
+        ChatRoute(
+            actionSnackbar = action
+        )
+    }
+}
