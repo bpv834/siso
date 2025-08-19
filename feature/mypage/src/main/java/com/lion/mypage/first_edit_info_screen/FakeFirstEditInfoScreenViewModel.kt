12ed905df3815b108e23afcb22bea25c401e0bb6
jaeminKimPea <kg8480@gmail.com>
2025-08-19 17:14:46 +0900

    FirstLoginInfoScreen.kt 화면 수정 관련 기능

--- feature/mypage/src/main/java/com/lion/mypage/first_edit_info_screen/FakeFirstEditInfoScreenViewModel.kt
+++ feature/mypage/src/main/java/com/lion/mypage/first_edit_info_screen/FakeFirstEditInfoScreenViewModel.kt
@@ -1,39 +1,41 @@
 package com.lion.mypage.first_edit_info_screen
 
+import android.util.Log.d
 import androidx.compose.runtime.mutableStateListOf
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class FakeFirstEditInfoScreenViewModel(
     // usecase자리
 ): FirstEditInfoScreenViewModelType {
     private val _nameState = MutableStateFlow("")
-    override val nameState : String get() = _nameState.value
+    override val nameState : StateFlow<String> get() = _nameState.asStateFlow()
     private val _ageState = MutableStateFlow("")
-    override val ageState: String get() = _ageState.value
-    private val _firstContinueBoolean = MutableStateFlow(true)
-    override val fistContinueBoolean : Boolean get() = _firstContinueBoolean.value
-    private val _myRadioButtons = MutableStateFlow(
-        mutableStateListOf(
-            Pair(first = "남성", second = true),
-            Pair(first = "여성", second = false),
-        )
-    )
-    override val myRadioButtons : MutableList<Pair<String, Boolean>> get() = _myRadioButtons.value
-    private val _pairRadioButtons = MutableStateFlow(
-        mutableStateListOf(
-            Pair(first = "동성", second = true),
-            Pair(first = "이성", second = false),
-        )
-    )
-    override val pairRadioButtons : MutableList<Pair<String, Boolean>> get() = _pairRadioButtons.value
-    override fun nameUpdate(input: String) = _nameState.update {
-        it + input
+    override val ageState: StateFlow<String> get() = _ageState.asStateFlow()
+    private val _introduceState = MutableStateFlow("")
+    override val introduceState: StateFlow<String> get() = _introduceState.asStateFlow()
+    private val _firstContinueBoolean = MutableStateFlow(false)
+    override val fistContinueBoolean : StateFlow<Boolean> get() = _firstContinueBoolean.asStateFlow()
+   override fun nameUpdate(input: String) = _nameState.update {
+        input
     }
 
-    override fun ageUpdate(input: String) = _nameState.update {
-        it + input
+    override fun ageUpdate(input: String) = _ageState.update {
+        input
     }
 
-    override fun fistContinueBooleanUpdate(input: Boolean) = _firstContinueBoolean.update { input }
+    override fun introduceUpdate(input: String) {
+        input
+    }
+
+    override fun fistContinueBooleanUpdate(nameNotBlank:Boolean,ageNotBlank: Boolean) = _firstContinueBoolean.update {
+        val textBoolean = nameNotBlank && ageNotBlank
+        d("boolean","text $ageState")
+        d("boolean","text $nameState")
+        d("boolean","text $textBoolean")
+
+        textBoolean
+    }
 }
