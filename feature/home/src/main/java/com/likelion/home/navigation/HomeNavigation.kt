40c3db8c6612bd69a6920e36e2070efdf0762e3e
zzangse1 <zzangse1014@gmail.com>
2025-08-19 10:08:33 +0900

    Chore: 패키지명 변경

--- feature/home/src/main/java/com/likelion/home/navigation/HomeNavigation.kt
+++ feature/home/src/main/java/com/likelion/home/navigation/HomeNavigation.kt
@@ -4,7 +4,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.likelion.home.home.HomeRoute
+import com.likelion.home.home_page.HomeRoute
 
 import com.likelion.navigation.NavigationRoute
 
--- feature/home/src/main/java/com/likelion/home/navigation/MyPageNavigation.kt
+++ feature/home/src/main/java/com/likelion/home/navigation/MyPageNavigation.kt
@@ -4,8 +4,6 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.likelion.home.chat.ChatRoute
-import com.likelion.home.home.HomeRoute
 import com.likelion.home.mypage.MyPageRoute
 import com.likelion.navigation.NavigationRoute
 
