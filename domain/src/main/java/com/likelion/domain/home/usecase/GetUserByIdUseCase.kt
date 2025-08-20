91eeb6f17fd79af8a2e79623e73a726a245008d5
ho <yungjoon2@naver.com>
2025-08-20 16:14:12 +0900

    Refactor : 단순 경로 변경

--- domain/src/main/java/com/likelion/domain/home/usecase/GetUserByIdUseCase.kt
+++ domain/src/main/java/com/likelion/domain/home/usecase/GetUserByIdUseCase.kt
@@ -1,7 +1,7 @@
-package com.likelion.domain.usecase
+package com.likelion.domain.home.usecase
 
-import com.likelion.domain.model.UsersModel
-import com.likelion.domain.repository.UsersRepository2
+import com.likelion.domain.home.model.UsersModel
+import com.likelion.domain.home.repository.UsersRepository2
 
 class GetUserByIdUseCase(
     private val usersRepository2: UsersRepository2
