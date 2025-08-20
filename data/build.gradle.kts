1913c9d173e402bb7f76c601fa237a4ae40da5da
bpv834 <yungjoon2@naver.com>
2025-08-21 00:47:10 +0900

    Feat : 테스트 관련 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -43,5 +43,14 @@ dependencies {
     // DI
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
+
+
+    // test
+    testImplementation(libs.junit)
+    // Kotlin Coroutines Test
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
+    // (옵션) Truth 같은 assertion 라이브러리
+    testImplementation("com.google.truth:truth:1.4.2")
+    implementation(kotlin("test"))
 }
 
