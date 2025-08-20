6fb47fb339e4de8b98f49796e21b780139ff67f3
zzangse1 <zzangse1014@gmail.com>
2025-08-20 09:43:23 +0900

    Refact: 로그인 버튼 UI 수정

--- feature/login/src/main/java/com/likelion/login/login_start/LoginScreen.kt
+++ feature/login/src/main/java/com/likelion/login/login_start/LoginScreen.kt
@@ -1,22 +1,17 @@
-package com.likelion.login.login
+package com.likelion.login.login_start
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
-import android.os.Build
-import android.util.Base64
 import android.util.Log
 import android.view.View
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,7 +21,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,9 +32,6 @@ import com.likelion.ui.R
 import com.likelion.ui.theme.SisoColorTokens
 import com.likelion.ui.theme.SisoTheme
 import com.likelion.ui.theme.SisoTypoTokens
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
-import kotlin.io.encoding.ExperimentalEncodingApi
 
 @Composable
 fun LoginRoute(
@@ -73,8 +64,6 @@ fun LoginScreen(
     }
     //val loginStatusInfoTitle = if (isLoggedIn.value) "로그인 상태" else "로그아웃 상태"
 
-    val context = LocalContext.current  // ✅ Context 가져오기
-    getKeyHash(context)
     Scaffold { innerPadding ->
         AsyncImage(
             contentScale = ContentScale.Crop,
@@ -82,14 +71,19 @@ fun LoginScreen(
             contentDescription = "바탕화면"
         )
 
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
+                .statusBarsPadding()
+                .verticalScroll(rememberScrollState())
+            ,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            Column {
+            Column(modifier = Modifier.fillMaxWidth()) {
                 Text(
-                    modifier = Modifier.padding(start = 24.dp, end = 54.dp, top = 120.dp),
+                    modifier = Modifier.padding(start = 24.dp, top = 120.dp),
                     text = "지금도,\n사랑하기 딱 좋은 나이",
                     style = SisoTypoTokens.H1,
                     color = SisoColorTokens.Orange100
@@ -102,15 +96,13 @@ fun LoginScreen(
                     color = SisoColorTokens.Orange100
                 )
             }
+
             AsyncImage(
                 model = R.drawable.kakao_login,
                 contentDescription = "",
                 modifier = Modifier
                     .fillMaxWidth()
-                    .align(Alignment.BottomCenter)
-                    .padding(horizontal = 16.dp)
-                    .navigationBarsPadding()
-                    .padding(bottom = 50.dp)
+                    .padding(horizontal = 16.dp, vertical = 32.dp)
                     .clickable(
                         indication = null,
                         interactionSource = remember { MutableInteractionSource() }
@@ -122,46 +114,6 @@ fun LoginScreen(
     }
 }
 
-@SuppressLint("PackageManagerGetSignatures")
-fun getKeyHash(context: Context): String? {
-    return try {
-        val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            context.packageManager.getPackageInfo(
-                context.packageName,
-                PackageManager.GET_SIGNING_CERTIFICATES
-            )
-        } else {
-            context.packageManager.getPackageInfo(
-                context.packageName,
-                PackageManager.GET_SIGNATURES
-            )
-        }
-
-        val signatures = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            packageInfo.signingInfo!!.apkContentsSigners
-        } else {
-            @Suppress("DEPRECATION")
-            packageInfo.signatures
-        }
-
-        if (signatures != null) {
-            for (signature in signatures) {
-                val md = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                val keyHash = android.util.Base64.encodeToString(
-                    md.digest(),
-                    android.util.Base64.NO_WRAP
-                )
-                Log.d("키해시", "앱의 디버그 키해시는 👉 $keyHash") // ✅ 한글 로그
-                return keyHash
-            }
-        }
-        null
-    } catch (e: Exception) {
-        Log.e("키해시", "키해시 생성 중 오류 발생: ${e.message}") // ✅ 한글 에러 로그
-        null
-    }
-}
 @Preview
 @Composable
 fun LoginScreenPreview() {
