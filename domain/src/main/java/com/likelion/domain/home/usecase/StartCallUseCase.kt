9e9b8e1d2e24e88aaf9e8fd6bcb24cb5ffa986f5
bpv834 <yungjoon2@naver.com>
2025-08-21 00:29:27 +0900

    Fix : call useCase 메서드 매개변수 추가 -callerId: Long, receiverId: Long

---
+++ domain/src/main/java/com/likelion/domain/home/usecase/StartCallUseCase.kt
@@ -0,0 +1,12 @@
+package com.likelion.domain.home.usecase
+
+import com.likelion.domain.home.repository.CallRepository
+import javax.inject.Inject
+
+class StartCallUseCase @Inject constructor(
+    private val callRepository: CallRepository
+) {
+    suspend fun execute(callerId: Long, receiverId: Long): String {
+        return callRepository.startCall(callerId = callerId, receiverId = receiverId)
+    }
+}
