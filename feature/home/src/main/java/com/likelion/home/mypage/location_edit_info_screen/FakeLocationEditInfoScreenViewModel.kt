3315b8e5ebd06c37cc6286f6de849fd0cc666b88
jaeminKimPea <kg8480@gmail.com>
2025-08-21 15:45:03 +0900

    위치 수정 페이지 fake 뷰모델

--- feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/FakeLocationEditInfoScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/FakeLocationEditInfoScreenViewModel.kt
@@ -1,7 +1,16 @@
 package com.likelion.home.mypage.location_edit_info_screen
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+
 class FakeLocationEditInfoScreenViewModel(
     // usecase자리
 ): LocationEditInfoScreenViewModelType {
-
+    private val _locationState = MutableStateFlow("")
+    override fun locationComplete(input: String, nav :()-> Unit){
+        _locationState.update { input }
+        if (_locationState.value.isNotBlank()){
+            nav()
+        }
+    }
 }
