a0f4a01e1fcf4ba1762a53f2999bbf11a521a2c7
jaeminKim <kg8480@gmail.com>
2025-08-22 02:50:08 +0900

    AdditionalInfoReligionScreen.kt ui 초기본

--- feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_religion_screen/AdditionalInfoReligionScreenViewModelType.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_religion_screen/AdditionalInfoReligionScreenViewModelType.kt
@@ -1,5 +1,9 @@
 package com.likelion.home.mypage.additional_info.additional_info_religion_screen
 
-interface AdditionalInfoReligionScreenViewModelType {
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
+interface AdditionalInfoReligionScreenViewModelType {
+    val receiverList : StateFlow<List<String>>
 }
