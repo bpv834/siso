3141a82349d8dddab5164e025eef27f63cec9ce4
bpv834 <yungjoon2@naver.com>
2025-08-22 00:05:01 +0900

    Fix : usecase 리턴타입 result<CallInfoModel> 로 변경 예외처리위함

--- domain/src/main/java/com/likelion/domain/home/usecase/StartCallUseCase.kt
+++ domain/src/main/java/com/likelion/domain/home/usecase/StartCallUseCase.kt
@@ -1,12 +1,13 @@
 package com.likelion.domain.home.usecase
 
+import com.likelion.domain.home.model.CallInfoModel
 import com.likelion.domain.home.repository.CallRepository
 import javax.inject.Inject
 
 class StartCallUseCase @Inject constructor(
     private val callRepository: CallRepository
 ) {
-    suspend fun execute(callerId: Long, receiverId: Long): String {
+    suspend fun execute(callerId: Long, receiverId: Long): Result<CallInfoModel> {
         return callRepository.startCall(callerId = callerId, receiverId = receiverId)
     }
 }
