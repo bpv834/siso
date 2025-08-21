c700f1ec55405bf2aec79576323e07cad38c2367
jaeminKim <kg8480@gmail.com>
2025-08-22 02:50:27 +0900

    AdditionalInfoSmokingScreen.kt ui 초기본

--- feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_smoking_screen/AdditionalInfoSmokingScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_smoking_screen/AdditionalInfoSmokingScreenViewModel.kt
@@ -2,11 +2,22 @@ package com.likelion.home.mypage.additional_info.additional_info_smoking_screen
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class AdditionalInfoSmokingScreenViewModel@Inject constructor (
+
     //usecase자리
 ): ViewModel(), AdditionalInfoSmokingScreenViewModelType {
-
+    private var _receiver = MutableStateFlow("")
+    init {
+        _receiver.update {
+            it// 받아올 값 추가
+        }
+    }
+    override val receiver: StateFlow<String> = _receiver.asStateFlow()
 }
