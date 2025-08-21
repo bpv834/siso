f53e929af3aa159f3ab013779f12e60b3ae17643
jaeminKimPea <kg8480@gmail.com>
2025-08-21 15:33:53 +0900

    속성 위치 변경

--- core/ui/src/main/java/com/likelion/ui/component/button/CommonActiveButton.kt
+++ core/ui/src/main/java/com/likelion/ui/component/button/CommonActiveButton.kt
@@ -1,16 +1,12 @@
 package com.likelion.ui.component.button
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.likelion.ui.theme.SisoColorTokens
@@ -20,8 +16,8 @@ import com.likelion.ui.theme.SisoTypoTokens
 @Composable
 fun CommonActiveButton(
     text: String,
+    modifier: Modifier = Modifier, // ✨ 외부에서 modifier를 받도록 설정
     onClick: () -> Unit,
-    modifier: Modifier = Modifier // ✨ 외부에서 modifier를 받도록 설정
 ) {
     OutlinedButton(
         onClick = onClick,
