6ce7a3048a0d335ffc71b644ca667b8f29c6bee9
jaeminKim <kg8480@gmail.com>
2025-08-22 02:54:18 +0900

    LocationEditInfoScreen.kt ui 초기본

--- feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/LocationEditInfoScreen.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/LocationEditInfoScreen.kt
@@ -3,6 +3,7 @@ package com.likelion.home.mypage.location_edit_info_screen
 import android.R.attr.end
 import android.content.res.Resources
 import android.util.Log.d
+import android.util.TypedValue
 import androidx.annotation.ArrayRes
 import com.likelion.ui.R
 import androidx.compose.foundation.background
@@ -12,10 +13,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -37,6 +40,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
@@ -52,18 +56,21 @@ import com.likelion.ui.theme.SisoTypoTokens
 fun LocationEditInfoScreen(
     viewModel: LocationEditInfoScreenViewModelType
 ) {
-
-    val text by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    var text by remember { mutableStateOf("") }
+    var tempText by remember { mutableStateOf("") }
     val locationTextFloat = if (text.isBlank())  0.845F else 1F
     var bottomState by remember { mutableStateOf(false) }
+    var topLocation by remember { mutableStateOf("") }
     val sheetState = rememberModalBottomSheetState(
         skipPartiallyExpanded = true,
     )
 
-    val topLocation = stringArrayResource(R.array.상위위치)
+    val topLocations = locationMapper.topLocationList
 
     Box(
         modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+            .fillMaxHeight()
     ) {
         Column(
             modifier = Modifier.verticalScroll(rememberScrollState())
@@ -78,24 +85,26 @@ fun LocationEditInfoScreen(
             Spacer(Modifier.size(24.dp))
             Box(
                 modifier = Modifier
-                    .height(54.dp).fillMaxWidth()
+                    .height(54.dp)
+                    .fillMaxWidth()
                     .background(SisoColorTokens.GrayScale20, RoundedCornerShape(999.dp))
                     .clickable(
                         indication = null,
                         interactionSource = remember { MutableInteractionSource() }
-                    ){
-                        if (text.isBlank()){
-                            bottomState = true
-                        }
+                    ) {
+                        // 바텀 올리기
+                        bottomState = true
+
                     },
             ){
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ){
                     Text(
-                        modifier = Modifier.padding(top = 15.5.dp, bottom = 15.5.dp, start = 16.dp)
+                        modifier = Modifier
+                            .padding(top = 15.5.dp, bottom = 15.5.dp, start = 16.dp)
                             .fillMaxWidth(locationTextFloat),
-                        text = "검색",
+                        text = text.ifBlank { "검색" },
                         style = SisoTypoTokens.Body2,
                         color = SisoColorTokens.GrayScale70,
                         textAlign = TextAlign.Start
@@ -103,19 +112,14 @@ fun LocationEditInfoScreen(
                     Spacer(Modifier.size(if (text.isNotBlank())3.dp
                     else 16.dp))
                     if (text.isNotBlank()) {
-                        IconButton(
-                            modifier = Modifier.padding(end = 16.dp),
-                            onClick = {
-                                bottomState = true
-                            }
-                        ){
-                            Icon(
-                                modifier = Modifier.size(24.dp),
-                                imageVector = ImageVector.vectorResource(R.drawable.text_edit),
-                                tint = SisoColorTokens.GrayScale40,
-                                contentDescription = ""
-                            )
-                        }
+                        Icon(
+                            modifier = Modifier.size(24.dp)
+                                .padding(end = 16.dp),
+                            imageVector = ImageVector.vectorResource(R.drawable.text_edit),
+                            tint = SisoColorTokens.GrayScale40,
+                            contentDescription = ""
+                        )
+
 
                     }
                 }
@@ -129,7 +133,7 @@ fun LocationEditInfoScreen(
                     indication = null,
                     interactionSource = remember { MutableInteractionSource() }
                 ){
-                    bottomState = true
+                    // 현재 위치로 설정하기 api
                 }
             ){
                 Icon(
@@ -154,7 +158,9 @@ fun LocationEditInfoScreen(
                 Modifier.align(Alignment.BottomCenter)
             ) {
                 CommonActiveButton(
-                    modifier = Modifier.fillMaxWidth().height(54.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(54.dp),
                     text = "완료하기",
                 ){
                     viewModel.locationComplete(input = text){
@@ -167,14 +173,25 @@ fun LocationEditInfoScreen(
         }
         if (bottomState)
             ModalBottomSheet(
+                modifier = Modifier.systemBarsPadding(),
                 onDismissRequest = {
+                    // 바텀 내리기
                     bottomState = false
+                    // 하위 지역 모드 초기화
+                    topLocation = ""
+                    // temp 초기화
+                    tempText = ""
+                    // 검색 버튼 텍스트 초기화
+                    text = ""
                 },
                 sheetState = sheetState,
                 dragHandle = null
             ) {
+                Spacer(Modifier.size(16.dp))
                 Box(
-                    modifier = Modifier.fillMaxWidth().height(31.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(31.dp),
                     contentAlignment = Alignment.Center
                 ) {
                     Text(
@@ -185,24 +202,58 @@ fun LocationEditInfoScreen(
                         textAlign = TextAlign.Center
                     )
                     IconButton(
-                        modifier = Modifier.padding(end = 20.dp)
+                        modifier = Modifier
+                            .padding(end = 20.dp)
                             .align(Alignment.CenterEnd),
                         onClick = {
+                            // 바텀 내리기
                             bottomState = false
+                            // 하위 지역 모드 초기화
+                            topLocation = ""
+                            // temp 초기화
+                            tempText = ""
+                            // 검색 버튼 텍스트 초기화
+                            text = ""
                         }
                     ){
                         Icon(
-                            modifier = Modifier.size(24.dp)
+                            modifier = Modifier
+                                .size(24.dp)
                                 .align(Alignment.CenterEnd),
-                            imageVector = ImageVector.vectorResource(R.drawable.text_edit),
-                            tint = SisoColorTokens.GrayScale40,
+                            imageVector = ImageVector.vectorResource(R.drawable.bottom_close),
+                            tint = SisoColorTokens.GrayScale90,
                             contentDescription = ""
                         )
                     }
                 }
                 Spacer(Modifier.size(31.dp))
-
-
+                if (topLocation.isBlank()) {
+                    topLocations.forEach {
+                        LocationText(input = it.first) {
+                            //바텀 내용 하위 지역 모드 설정
+                            topLocation = it.second
+                            // 상위 지역 텍스트 임시 저장
+                            tempText = it.first
+                        }
+                    }
+                }else{
+                    val resId = context.resources.getIdentifier(
+                        "city_${ topLocation }",// 선택된 상위 위치
+                        "array",
+                        context.packageName
+                    )
+                    val locations = context.resources.getStringArray(resId)
+                    locations.forEach {
+                        LocationText(input = it) {
+                            // 하위 지역 추가
+                            text = "$tempText $it"
+                            //바텀 내용 하위 지역 모드 초기화
+                            topLocation = ""
+                            // 바텀 내리기
+                            bottomState = false
+                        }
+                    }
+                }
 
             }
     }
@@ -216,14 +267,14 @@ fun LocationText(
 ){
     Column (
         modifier = Modifier
-            .clip(RoundedCornerShape(3.dp))
             .clickable{
                 option()
             }
     ){
         Spacer(modifier = Modifier.size(16.dp))
         Text(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
                 .padding(start = 16.dp),
             text = input,
             style = SisoTypoTokens.Body2,
