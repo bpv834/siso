e47aa9b01b2e8af3316aeb30c4847672f10aa0e4
jaeminKim <kg8480@gmail.com>
2025-08-22 02:53:08 +0900

    InterestEditInfoScreen.kt ui 초기본

--- feature/home/src/main/java/com/likelion/home/mypage/interest_edit_info_screen/InterestEditInfoScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/interest_edit_info_screen/InterestEditInfoScreenViewModel.kt
@@ -2,11 +2,21 @@ package com.likelion.home.mypage.interest_edit_info_screen
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class InterestEditInfoScreenViewModel @Inject constructor (
     //usecase자리
 ): ViewModel(), InterestEditInfoScreenViewModelType {
-
+    private var _receiverList = MutableStateFlow(listOf<String>())
+    init {
+        // 초기 리스트 불러오는 부분
+        _receiverList.update {
+            listOf<String>()
+        }
+    }
+    override val receiverList = _receiverList.asStateFlow()
 }
