c51823e06f532f9dc11a543ea54be0b26cd7a210
jaeminKim <kg8480@gmail.com>
2025-08-22 02:54:31 +0900

    LocationEditInfoScreen.kt mapper

---
+++ feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/locationMapper.kt
@@ -0,0 +1,23 @@
+package com.likelion.home.mypage.location_edit_info_screen
+
+object locationMapper {
+    val topLocationList = listOf(
+        "서울" to "seoul",
+        "인천" to "incheon",
+        "경기" to "gyeonggi",
+        "강원" to "gangwon",
+        "대전" to "daejeon",
+        "세종" to "sejong",
+        "충북" to "chungbuk",
+        "충남" to "chungnam",
+        "광주" to "gwangju",
+        "전북" to "jeonbuk",
+        "전남" to "jeonnam",
+        "대구" to "daegu",
+        "경북" to "gyeongbuk",
+        "부산" to "busan",
+        "울산" to "ulsan",
+        "경남" to "gyeongnam",
+        "제주" to "jeju",
+    )
+}
