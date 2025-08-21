30081be1a62c9a20ba55575c3bac5e70bdf9e705
jaeminKimPea <kg8480@gmail.com>
2025-08-21 15:46:15 +0900

    위치 수정 페이지 뷰모델

--- feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/LocationEditInfoScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/LocationEditInfoScreenViewModel.kt
@@ -1,12 +1,22 @@
 package com.likelion.home.mypage.location_edit_info_screen
 
+import android.R.id.input
+import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class LocationEditInfoScreenViewModel @Inject constructor (
     //usecase자리
 ): ViewModel(), LocationEditInfoScreenViewModelType {
-
+    private val _locationState = MutableStateFlow("")
+    override fun locationComplete(input: String, nav :()-> Unit){
+        _locationState.update { input }
+        if (_locationState.value.isNotBlank()){
+            nav()
+        }
+    }
 }
