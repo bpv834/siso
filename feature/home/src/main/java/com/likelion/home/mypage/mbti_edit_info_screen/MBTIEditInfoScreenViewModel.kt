507dadfc0b761522eb64379bc5d9ab519b305234
jaeminKim <kg8480@gmail.com>
2025-08-22 02:55:28 +0900

    MBTIEditInfoScreen.kt 초기본

--- feature/home/src/main/java/com/likelion/home/mypage/mbti_edit_info_screen/MBTIEditInfoScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/mbti_edit_info_screen/MBTIEditInfoScreenViewModel.kt
@@ -2,11 +2,21 @@ package com.likelion.home.mypage.mbti_edit_info_screen
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class MBTIEditInfoScreenViewModel @Inject constructor (
     //usecase자리
 ): ViewModel(), MBTIEditInfoScreenViewModelType {
-
+    private var _receiver = MutableStateFlow("")
+    init {
+        _receiver.update {
+            it// 받아올 값 추가
+        }
+    }
+    override val receiver: StateFlow<String> = _receiver.asStateFlow()
 }
