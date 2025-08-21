a7fc51ae4d6db23e41b93a38c11527343311cf8e
jaeminKim <kg8480@gmail.com>
2025-08-22 02:49:39 +0900

    AdditionalInfoAlcoholScreen.kt ui 초기본

--- feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_alcohol_screen/AdditionalInfoAlcoholScreenViewModelType.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_alcohol_screen/AdditionalInfoAlcoholScreenViewModelType.kt
@@ -1,5 +1,7 @@
 package com.likelion.home.mypage.additional_info.additional_info_alcohol_screen
 
-interface AdditionalInfoAlcoholScreenViewModelType {
+import kotlinx.coroutines.flow.StateFlow
 
+interface AdditionalInfoAlcoholScreenViewModelType {
+    val receiver : StateFlow<String>
 }
