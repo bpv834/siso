a8d97db88272862995ac3800abf48254594b2015
jaeminKim <kg8480@gmail.com>
2025-08-22 02:57:39 +0900

    FakeAdditionalInfoReligionScreenViewModel.kt 초기본

---
+++ feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_religion_screen/FakeAdditionalInfoReligionScreenViewModel.kt
@@ -0,0 +1,18 @@
+package com.likelion.home.mypage.additional_info.additional_info_religion_screen
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class FakeAdditionalInfoReligionScreenViewModel(
+    // usecase자리
+): AdditionalInfoReligionScreenViewModelType {
+    private var _receiverList = MutableStateFlow(listOf<String>())
+    init {
+        // 초기 리스트 불러오는 부분
+        _receiverList.update {
+            listOf<String>()
+        }
+    }
+    override val receiverList = _receiverList.asStateFlow()
+}
