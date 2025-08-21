784ae27266faeff17da7eefee6d51e054870c9eb
jaeminKim <kg8480@gmail.com>
2025-08-22 02:52:11 +0900

    AdditionalInfoSmokingScreen.kt ui 초기본

--- feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_smoking_screen/FakeAdditionalInfoSmokingScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_smoking_screen/FakeAdditionalInfoSmokingScreenViewModel.kt
@@ -1,7 +1,18 @@
 package com.likelion.home.mypage.additional_info.additional_info_smoking_screen
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
 class FakeAdditionalInfoSmokingScreenViewModel(
     // usecase자리
 ): AdditionalInfoSmokingScreenViewModelType {
-
+    private var _receiver = MutableStateFlow("")
+    init {
+        _receiver.update {
+            it// 받아올 값 추가
+        }
+    }
+    override val receiver: StateFlow<String> = _receiver.asStateFlow()
 }
