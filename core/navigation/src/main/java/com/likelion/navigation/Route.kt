7adc10959ff7a05b2c52eabc78c01eff560e6487
jaeminKimPea <kg8480@gmail.com>
2025-08-22 16:38:54 +0900

    잘못 삽입한 네비 지움

--- core/navigation/src/main/java/com/likelion/navigation/Route.kt
+++ core/navigation/src/main/java/com/likelion/navigation/Route.kt
@@ -7,9 +7,7 @@ sealed class NavigationRoute(val route: String) {
     object HomeScreen : NavigationRoute("home")
     object FindScreen : NavigationRoute("find")
     object ChatScreen : NavigationRoute("chat")
-    object MyPageScreen : NavigationRoute("myPage"){
-        object MainEditScreen : NavigationRoute("myPage/main_edit")
-    }
+    object MyPageScreen : NavigationRoute("myPage")
     /*
     * object 보여줄화면: NavigationRoute("보여줄화면")
     *
