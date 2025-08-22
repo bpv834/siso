cb997323b75a7c850c1febcda621cfd2aabeb74a
jaeminKimPea <kg8480@gmail.com>
2025-08-22 11:56:54 +0900

    MainEditInfoScreen.kt 네비 삽입

--- core/navigation/src/main/java/com/likelion/navigation/Route.kt
+++ core/navigation/src/main/java/com/likelion/navigation/Route.kt
@@ -8,7 +8,9 @@ sealed class NavigationRoute(val route: String) {
     object ChatScreen : NavigationRoute("chat") {
         object AlarmScreen : NavigationRoute("chat/alarm")
     }
-    object MyPageScreen : NavigationRoute("myPage")
+    object MyPageScreen : NavigationRoute("myPage"){
+        object MainEditScreen : NavigationRoute("myPage/main_edit")
+    }
     /*
     * object 보여줄화면: NavigationRoute("보여줄화면")
     *
