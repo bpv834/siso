3c99c27bf86ce042134fdd707fa4cd63e8e091c0
zzangse1 <zzangse1014@gmail.com>
2025-08-23 18:38:39 +0900

    Feat: Chat 화면 네비게이션 연결

--- feature/home/src/main/java/com/likelion/home/navigation/ChatNavigation.kt
+++ feature/home/src/main/java/com/likelion/home/navigation/ChatNavigation.kt
@@ -4,18 +4,33 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.likelion.home.chat.AlarmRoute
 import com.likelion.home.chat.ChatRoute
 import com.likelion.navigation.NavigationRoute
 
 fun NavController.navigateToChat(navOptions: NavOptions? = null) =
     navigate(NavigationRoute.ChatScreen.route, navOptions)
 
-fun NavGraphBuilder.chatNavigation(action: () -> Unit) {
+fun NavGraphBuilder.chatNavigation(
+    navController: NavController,
+    onNavigateUp: () -> Unit,
+    action: () -> Unit
+) {
     composable(
         route = NavigationRoute.ChatScreen.route
     ) {
         ChatRoute(
+            onNavigateAlarm = {
+                navController.navigate(NavigationRoute.ChatScreen.AlarmScreen.route) {
+                    launchSingleTop = true
+                }
+            },
             actionSnackbar = action
+        )
+    }
+    composable(NavigationRoute.ChatScreen.AlarmScreen.route) {
+        AlarmRoute(
+            onNavigateUp = { navController.popBackStack() }
         )
     }
 }
