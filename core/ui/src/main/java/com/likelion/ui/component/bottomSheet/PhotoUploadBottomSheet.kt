62f6b6d570bdc37e656ee436fb0fb44e4912fb56
bpv834 <yungjoon2@naver.com>
2025-08-24 14:18:23 +0900

    Fix : 사진 바텀시트, 사진섹션, 사진런처 및 업로드 캡슐화

---
+++ core/ui/src/main/java/com/likelion/ui/component/bottomSheet/PhotoUploadBottomSheet.kt
@@ -0,0 +1,119 @@
+package com.likelion.ui.component.bottomSheet
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.likelion.ui.R
+import com.likelion.ui.component.button.CommonActiveButton
+import com.likelion.ui.theme.SisoColorTokens
+import com.likelion.ui.theme.SisoTheme
+import com.likelion.ui.theme.SisoTypoTokens
+
+
+// 💡 PhotoUploadBottomSheet Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun PhotoUploadBottomSheetPreview() {
+    SisoTheme {
+        PhotoUploadBottomSheet(
+            onDismissRequest = {},
+            onTakePhotoClick = {},
+            onPickFromGalleryClick = {}
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PhotoUploadBottomSheet(
+    onDismissRequest: () -> Unit,
+    onTakePhotoClick: () -> Unit,
+    onPickFromGalleryClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp, vertical = 16.dp)
+    ) {
+        Spacer(modifier = Modifier.height(22.dp))
+
+        // 상단 헤더 (제목과 닫기 버튼)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "사진 업로드 도움말",
+                style = SisoTypoTokens.Title2
+            )
+            IconButton(onClick = onDismissRequest) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_close_24px),
+                    contentDescription = "Close"
+                )
+            }
+        }
+
+        // 도움말 텍스트
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = "꼭 얼굴이 아니더라도 내가 관심있는 분야의\n사진을 올려줘도 좋아요",
+            style = SisoTypoTokens.Body4,
+            color = SisoColorTokens.GrayScale60
+        )
+        Text(
+            text = "예시) 반려동물, 꽃, 운동하는 사진, 등산 등",
+            style = SisoTypoTokens.Body4,
+            color = Color.Gray
+        )
+
+        // 예시 이미지들
+        Spacer(modifier = Modifier.height(24.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            AsyncImage(
+                model = R.drawable.img_dog,
+                contentDescription = "",
+                modifier = Modifier.size(105.dp, 106.dp)
+            )
+            AsyncImage(
+                model = R.drawable.img_flower,
+                contentDescription = "",
+                modifier = Modifier.size(105.dp, 106.dp)
+            )
+            AsyncImage(
+                model = R.drawable.img_baseball,
+                contentDescription = "",
+                modifier = Modifier.size(105.dp, 106.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(60.dp))
+        // 하단 버튼
+        CommonActiveButton("카메라로 사진찍기", onClick = onTakePhotoClick)
+        Spacer(modifier = Modifier.height(8.dp))
+        CommonActiveButton("앨범에서 가져오기", onClick = onPickFromGalleryClick)
+        Spacer(modifier = Modifier.height(16.dp)) // 하단 패딩
+    }
+}
--- feature/login/src/main/java/com/likelion/login/login_input_photo/ThirdLoginInfoScreen.kt
+++ feature/login/src/main/java/com/likelion/login/login_input_photo/ThirdLoginInfoScreen.kt
@@ -1,49 +1,28 @@
 package com.likelion.login.login_input_photo
 
-import android.Manifest
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.net.Uri
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import com.likelion.ui.R
 import com.likelion.ui.component.button.CommonActiveButton
-import com.likelion.ui.component.button.CommonButtonWithState
-import com.likelion.ui.component.camera.CameraPreview
-import com.likelion.ui.component.photo_layout.PhotoLayoutWith1Main4Sub
+import com.likelion.ui.component.photo_layout.PhotoDisplaySection
+import com.likelion.ui.component.photo_layout.PhotoUploader
 import com.likelion.ui.component.text_button.CommonTextButton
 import com.likelion.ui.theme.SisoColorTokens
 import com.likelion.ui.theme.SisoTheme
@@ -56,42 +35,10 @@ fun ThirdLoginInfoScreen(
     viewModel: ThirdLoginInfoScreenViewModelType,
     onNavigateNext: () -> Unit
 ) {
-    val showBottomSheet by viewModel.showBottomSheet.collectAsStateWithLifecycle()
-    // 💡 바텀 시트 상태 수정:
-    // skipPartiallyExpanded = true: 부분 확장 상태를 건너뜁니다.
-    // initialValue = SheetValue.Expanded: 바텀 시트가 처음부터 확장된 상태로 열립니다.
-    val sheetState = rememberModalBottomSheetState(
-        skipPartiallyExpanded = true,
-    )
-    val context = LocalContext.current
-    // 카메라 노출 여부 상태 변수
-    var isCameraVisible by remember { mutableStateOf(false) }
-    // 가져온 비트맵 저장하는 리스트 변수
+    // 캡쳐된 이미지 보관 변수
     val capturedImages by viewModel.capturedImages.collectAsStateWithLifecycle()
-
-    // 카메라 권한 요청 런처
-    val cameraPermissionLauncher = rememberLauncherForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            // 권한이 트루면 카메라 보여주는 상태를 트루로 변경한다.
-            isCameraVisible = true
-        } else {
-            // 권한 거부 시 사용자에게 알림 로직
-        }
-    }
-
-    // 갤러리 이미지 선택 런처
-    val galleryLauncher = rememberLauncherForActivityResult(
-        ActivityResultContracts.GetContent()
-    ) { uri ->
-        if (uri != null) {
-            val bitmap = uri.getBitmap(context) // 확장 함수 사용
-            if (capturedImages.size < 5 && bitmap != null) {
-                viewModel.addImageFromAlbum(newImage = bitmap)
-            }
-        }
-    }
+    // 바텀시트 상태 관리 변수
+    val showBottomSheetState by viewModel.showBottomSheet.collectAsStateWithLifecycle()
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -112,33 +59,18 @@ fun ThirdLoginInfoScreen(
             style = SisoTypoTokens.Body4,
             color = SisoColorTokens.GrayScale60
         )
-        Spacer(modifier = Modifier.size(size = 12.dp))
-        Text(text = "대표사진", style = SisoTypoTokens.SubTitle1, color = SisoColorTokens.GrayScale55)
         Spacer(modifier = Modifier.size(size = 9.dp))
         // 불러온 비트맵 보여주는 리스트
-        PhotoLayoutWith1Main4Sub(
-            mainWith = null,
-            mainHeight = 206.dp,
-            subWith = 76.dp,
-            subHeight = 72.dp,
+        // 초기 이미지가 없다면 홀더를 보여줌
+        PhotoDisplaySection(
             capturedImages = capturedImages,
-            onClickDelete = { bitmap -> viewModel.deleteBitMap(bitmap) }
+            // 시트 보이게 하는 로직
+            onAddButtonClick = { viewModel.showPhotoUploadBottomSheet() },
+            onDelete = { bitmap -> viewModel.deleteBitMap(bitmap) },
         )
-        Spacer(modifier = Modifier.size(size = 68.dp))
-        // derivedStateOf는 다른 상태에서 파생된 값을 안전하고 효율적으로 계산하고 싶을 때 쓰는 도구예요.
-        val isAddCapture by remember { derivedStateOf { capturedImages.size < 5 } }
-        CommonButtonWithState (text = "사진 추가하기 (${capturedImages.size}/5)",
-            onClick = {
-                if (capturedImages.size < 5) {
-                    viewModel.showPhotoUploadBottomSheet()
-                    //onNavigateNext()
-                } else {
-                    // 5장 초과 시 처리 로직
-                }
-            },
-            isActive = isAddCapture
-        )
-        Spacer(Modifier.size(8.dp))
+        // 건너띄기가 보일땐 24로 사진추가,다음으로 버튼일땐 8로 가깝게
+        val spaceSize = if(capturedImages.size!=0) 8.dp else 24.dp
+        Spacer(Modifier.size(spaceSize))
         if (capturedImages.isEmpty()) // 이미지가 없을때만 건너뛰기를 노출
             Column(
                 modifier = Modifier.fillMaxWidth(),
@@ -153,165 +85,32 @@ fun ThirdLoginInfoScreen(
             }
         // 사진이 한개라도 있다면 다음으로 버튼 노출
         if (capturedImages.isNotEmpty())
-            CommonActiveButton(text = "다음으로", onClick = {onNavigateNext()})
+            CommonActiveButton(text = "다음으로", onClick = { onNavigateNext() })
     }
+    // 사진 추가 바텀시트 및 카메라, 앨범 런처
+    PhotoUploader(
+        onDismissRequest = {
+            viewModel.hidePhotoUploadBottomSheet()
+        },
+        // 갤러리에서 사진 가져와 images에 추가하는 메서드
+        onImagePicked = { bitmap ->
+            viewModel.addImageToCaptures(bitmap)
+        },
+        onImageCaptured = {bitmap ->
+            viewModel.addImageToCaptures(bitmap)
 
-    if (showBottomSheet) {
-        ModalBottomSheet(
-            onDismissRequest = {
-                viewModel.hidePhotoUploadBottomSheet()
-            },
-            sheetState = sheetState,
-            dragHandle = null,
-            containerColor = Color.White
-        ) {
-            PhotoUploadBottomSheet(
-                // 시트 닫기
-                onDismissRequest = {
-                    viewModel.hidePhotoUploadBottomSheet()
-                },
-                // 카메라 권한 실행
-                onTakePhotoClick = {
-                    viewModel.hidePhotoUploadBottomSheet()
-                    cameraPermissionLauncher.launch(Manifest.permission.CAMERA)
-                },
-                onPickFromGalleryClick = {
-                    viewModel.hidePhotoUploadBottomSheet()
-                    galleryLauncher.launch("image/*")
-                }
-            )
-        }
-    }
-
-
-    // 권한을 받아 상태를 변경한다면 카메라 프리뷰를 띄운다.
-    if (isCameraVisible) {
-        CameraPreview(
-            onImageCaptured = { bitmap ->
-                if (capturedImages.size < 5) {
-                    viewModel.addImageFromAlbum(newImage = bitmap)
-                }
-                isCameraVisible = false
-            }
-        )
-    }
+        },
+        showBottomSheet = showBottomSheetState
+    )
 }
 
-// --- 프리뷰 영역 ---
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
-@Composable
-fun ThirdLoginInfoScreenPreview() {
-    SisoTheme {
-        // 프리뷰를 위한 가상의 ViewModel
-        val viewModel = FakeThirdLoginScreenViewModel(LocalContext.current)
-        ThirdLoginInfoScreen(
-            viewModel = viewModel,
-            onNavigateNext = {}
-        )
-    }
-}
-
-// 💡 PhotoUploadBottomSheet Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
-fun PhotoUploadBottomSheetPreview() {
+fun ThirdLoginInfoScreenPreview() {
     SisoTheme {
-        PhotoUploadBottomSheet(
-            onDismissRequest = {},
-            onTakePhotoClick = {},
-            onPickFromGalleryClick = {}
-        )
+        val context = LocalContext.current
+        val fakeViewModel = FakeThirdLoginScreenViewModel(context)
+        ThirdLoginInfoScreen(fakeViewModel) { }
     }
 }
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun PhotoUploadBottomSheet(
-    onDismissRequest: () -> Unit,
-    onTakePhotoClick: () -> Unit,
-    onPickFromGalleryClick: () -> Unit
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 24.dp, vertical = 16.dp)
-    ) {
-        Spacer(modifier = Modifier.height(22.dp))
-
-        // 상단 헤더 (제목과 닫기 버튼)
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "사진 업로드 도움말",
-                style = SisoTypoTokens.Title2
-            )
-            IconButton(onClick = onDismissRequest) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_close_24px),
-                    contentDescription = "Close"
-                )
-            }
-        }
-
-        // 도움말 텍스트
-        Spacer(modifier = Modifier.height(24.dp))
-        Text(
-            text = "꼭 얼굴이 아니더라도 내가 관심있는 분야의\n사진을 올려줘도 좋아요",
-            style = SisoTypoTokens.Body4,
-            color = SisoColorTokens.GrayScale60
-        )
-        Text(
-            text = "예시) 반려동물, 꽃, 운동하는 사진, 등산 등",
-            style = SisoTypoTokens.Body4,
-            color = Color.Gray
-        )
-
-        // 예시 이미지들
-        Spacer(modifier = Modifier.height(24.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            AsyncImage(
-                model = R.drawable.img_dog,
-                contentDescription = "",
-                modifier = Modifier.size(105.dp, 106.dp)
-            )
-            AsyncImage(
-                model = R.drawable.img_flower,
-                contentDescription = "",
-                modifier = Modifier.size(105.dp, 106.dp)
-            )
-            AsyncImage(
-                model = R.drawable.img_baseball,
-                contentDescription = "",
-                modifier = Modifier.size(105.dp, 106.dp)
-            )
-        }
-
-        Spacer(modifier = Modifier.height(60.dp))
-        // 하단 버튼
-        CommonActiveButton("카메라로 사진찍기", onClick = onTakePhotoClick)
-        Spacer(modifier = Modifier.height(8.dp))
-        CommonActiveButton("앨범에서 가져오기", onClick = onPickFromGalleryClick)
-        Spacer(modifier = Modifier.height(16.dp)) // 하단 패딩
-    }
-}
-
-// uri를 비트맵으로 변환하는 확장 함수
-fun Uri.getBitmap(context: Context): Bitmap? {
-    return try {
-        val inputStream = context.contentResolver.openInputStream(this)
-        BitmapFactory.decodeStream(inputStream)
-    } catch (e: Exception) {
-        null
-    }
-}
-
-// ... (나머지 Preview 및 PhotoUploadBottomSheet 코드는 동일)
