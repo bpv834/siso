c8b319405697372047240b74a620ba20c515d047
jaeminKimPea <kg8480@gmail.com>
2025-08-25 16:51:15 +0900

    위치 수정 스크린 view 모델 수정

--- feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/FakeLocationEditInfoScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/FakeLocationEditInfoScreenViewModel.kt
@@ -1,8 +1,8 @@
 package com.likelion.home.mypage.location_edit_info_screen
 
 import androidx.lifecycle.ViewModel
-import com.likelion.domain.mypage.usecase.BottomLocationUseCaseImpl
-import com.likelion.domain.mypage.usecase.TopLocationUseCaseImpl
+import com.likelion.domain.mypage.usecase.BottomLocationUseCase
+import com.likelion.domain.mypage.usecase.TopLocationUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -10,11 +10,11 @@ import javax.inject.Inject
 
 class FakeLocationEditInfoScreenViewModel (
     // usecase자리
-    private val topLocationUseCaseImpl: TopLocationUseCaseImpl,
-    private val bottomLocationUseCaseImpl: BottomLocationUseCaseImpl
+    private val topLocationUseCase: TopLocationUseCase,
+    private val bottomLocationUseCase: BottomLocationUseCase
 ): LocationEditInfoScreenViewModelType {
     private val _locationState = MutableStateFlow("")
-    private val _topLocation = MutableStateFlow(topLocationUseCaseImpl.invoke())
+    private val _topLocation = MutableStateFlow(topLocationUseCase.invoke())
     val topLocation = _topLocation.asStateFlow()
     override fun locationComplete(input: String, nav :()-> Unit){
         _locationState.update { input }
