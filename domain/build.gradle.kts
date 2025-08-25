97ede257623569366ba10ceb2af7c6e076a1a35e
jaeminKimPea <kg8480@gmail.com>
2025-08-25 16:52:56 +0900

    fake repo 를 위한 Gson 삽입

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -20,7 +20,8 @@ dependencies {
     implementation(libs.hilt.core)
     ksp(libs.hilt.compiler)
     testImplementation(libs.junit)
-
+    // Gson 라이브러리
+    implementation(libs.gson.v2131)
 
     // Kotlin Coroutines Test
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
