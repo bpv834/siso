339a086a0757f313b3bdd52bd8b3596262bbe08b
jaeminKim <kg8480@gmail.com>
2025-08-26 02:19:56 +0900

    json 적용한 repo

--- domain/src/main/java/com/likelion/domain/mypage/repository/FakeLocationRepositoryImpl.kt
+++ domain/src/main/java/com/likelion/domain/mypage/repository/FakeLocationRepositoryImpl.kt
@@ -3,60 +3,62 @@ package com.likelion.domain.mypage.repository
 import com.google.gson.Gson
 import com.likelion.domain.mypage.model.Location
 import javax.inject.Inject
+import kotlin.collections.filter
+import kotlin.collections.map
 import kotlin.jvm.java
 
 class FakeLocationRepositoryImpl @Inject constructor(
-    private val locationMapper: FakeLocationMapper
+    json: String
 ): LocationRepository {
-    private val locationList =
-        Gson().fromJson("korea_regions_ordered.json", FakeLocationEntity::class.java)
 
-    override fun getTopLocationList(): Location {
-        return locationMapper.mapEntityToTopLocation(locationList)
+    private var locationList = FakeLocationEntity(listOf())
+    init {
+        setJson(json)
+    }
+    override fun setJson(json: String) {
+        locationList = Gson().fromJson(json, FakeLocationEntity::class.java)
     }
 
-    override fun getBottomLocationList(topName: String): List<Location> {
-        return locationMapper.mapEntityToBottomLocation(locationList, topName)
+    override fun getTopLocationList(): Location {
+        return locationList.toTopDomain()
+    }
+
+    override fun getBottomLocationList(topName: String): Location {
+        return locationList.toBottomDomain(topName)
     }
 }
 
-class FakeLocationMapper {
-    // 하위 -> 상위
-    fun mapEntityToTopLocation(entity: FakeLocationEntity): Location {
-        return Location(entity.locationList.map {
-            it.topName
-        })
+fun FakeLocationEntity.toTopDomain(): Location{
+    // 변환 하위 -> 상위
+    return Location(locationList.map {
+        it.topName
+    })
+}
 
-    }
-    fun mapEntityToBottomLocation(
-        entity: FakeLocationEntity,
-        topName: String
-    ): List<Location> {
-        val locationList = mutableListOf<Location>()
-        entity.locationList.forEach {
-            if (it.topName == topName) {
-                locationList.add(Location(it.bottomName))
-                return@forEach
-            }
-        }
-        return locationList
-    }
-    // 상위 -> 하위
-    fun mapDomainToEntity(domain: String,bottomName: List<String>){
-        //
+fun FakeLocationEntity.toBottomDomain(
+    topName: String
+): Location{
+    // 변환 하위 -> 상위
+    return Location(
+        name = locationList.filter {
+            it.topName == topName
+        }.map {
+            it.bottomName
+        }.first()
+    )
+}
 
-    }
+// 상위 -> 하위
+fun Location.mapDomainToEntity(domain: String,bottomName: List<String>){
+    //
+
 }
 
 data class FakeLocationEntity(
-    val locationList: List<FakeLocationListEntity>, // List<String>
-) {
-
-}
+    val locationList: List<FakeLocationListEntity>
+)
 
 data class FakeLocationListEntity(
-    val topName: String, // VARCHAR(255), NOT NULL
-    val bottomName: List<String>, // VARCHAR(255), NOT NULL
-) {
-
-}
+    val bottomName: List<String>,
+    val topName: String
+)
