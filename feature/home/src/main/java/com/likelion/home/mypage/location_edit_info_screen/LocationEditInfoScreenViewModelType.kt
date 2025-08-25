19d1ccc58c6078d5a0cad881990abaa5a23a7d51
jaeminKim <kg8480@gmail.com>
2025-08-26 02:18:17 +0900

    유즈케이스 적용 하여 수정한 뷰모델

--- feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/LocationEditInfoScreenViewModelType.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/LocationEditInfoScreenViewModelType.kt
@@ -1,5 +1,13 @@
 package com.likelion.home.mypage.location_edit_info_screen
 
+import com.likelion.domain.mypage.model.Location
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 interface LocationEditInfoScreenViewModelType {
-    fun locationComplete(input: String, nav :()-> Unit)
+    val topLocation: StateFlow<Location>
+    val bottomLocation:StateFlow<Location>
+    fun setBottomLocation(input: String)
+    fun locationComplete(input: String, nav :(String)-> Unit)
 }
