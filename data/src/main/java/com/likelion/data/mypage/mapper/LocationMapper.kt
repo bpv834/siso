53e338dc29f10ff4f4adf2b5740f643c6fc553d0
jaeminKimPea <kg8480@gmail.com>
2025-08-25 16:47:02 +0900

    locationMapper 수정

--- data/src/main/java/com/likelion/data/mypage/mapper/LocationMapper.kt
+++ data/src/main/java/com/likelion/data/mypage/mapper/LocationMapper.kt
@@ -3,27 +3,32 @@ package com.likelion.data.mypage.mapper
 import com.likelion.data.mypage.model.LocationEntity
 import com.likelion.domain.mypage.model.Location
 
-class LocationMapper {
-    // 하위 -> 상위
-    fun mapEntityToTopLocation(entity: LocationEntity): Location {
-        return Location(entity.locationList.map {
-            it.topName
-        })
 
-    }
-    fun mapEntityToBottomLocation(
-        entity: LocationEntity,
-        topName: String
-    ): List<Location> {
-        val locationList = mutableListOf<Location>()
-        entity.locationList.forEach {
-            if (it.topName == topName) {
-                locationList.add(Location(it.bottomName))
-                return@forEach
-            }
+fun LocationEntity.toTopDomain(): Location{
+    // 변환 하위 -> 상위
+    return Location(locationList.map {
+        it.topName
+    })
+}
+
+fun LocationEntity.toBottomDomain(
+    entity: LocationEntity,
+    topName: String
+): List<Location>{
+    // 변환 하위 -> 상위
+    locationList
+    val locationList = mutableListOf<Location>()
+    entity.locationList.forEach {
+        if (it.topName == topName) {
+            locationList.add(Location(it.bottomName))
+            return@forEach
         }
-        return locationList
     }
+    return locationList
+}
+
+
+class LocationMapper {
     // 상위 -> 하위
     fun mapDomainToEntity(domain: String,bottomName: List<String>){
         //
