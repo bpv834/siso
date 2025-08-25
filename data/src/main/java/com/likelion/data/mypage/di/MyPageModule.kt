bec9a1df82ed614f7b19c0b28539fbc743fde795
jaeminKimPea <kg8480@gmail.com>
2025-08-25 16:44:36 +0900

    mypage 모듈 수정

--- data/src/main/java/com/likelion/data/mypage/di/MyPageModule.kt
+++ data/src/main/java/com/likelion/data/mypage/di/MyPageModule.kt
@@ -3,6 +3,7 @@ package com.likelion.data.mypage.di
 import com.likelion.data.mypage.mapper.LocationMapper
 import com.likelion.data.mypage.repository.LocationRepositoryImpl
 import com.likelion.domain.mypage.repository.LocationRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,15 +12,20 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object MyPageModule {
-    @Provides
-    @Singleton
-    fun provideLocationMapper(): LocationMapper =
-        LocationMapper()
+abstract class MyPageModule {
+//    @Binds
+//    @Singleton
+//    fun provideLocationMapper(): LocationMapper =
+//        LocationMapper()
+//
+//    @Binds
+//    @Singleton
+//    fun provideMyPageRepository(
+//        locationMapper: LocationMapper
+//    ): LocationRepository = LocationRepositoryImpl(locationMapper)
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideMyPageRepository(
-        locationMapper: LocationMapper
-    ): LocationRepository = LocationRepositoryImpl(locationMapper)
+    abstract fun bindLocattionRepo(impl: LocationRepositoryImpl): LocationRepository
+
 }
