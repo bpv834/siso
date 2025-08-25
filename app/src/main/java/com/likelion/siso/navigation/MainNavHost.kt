2e005cdcf092c02a21c38269a927fada0f77451e
bpv834 <yungjoon2@naver.com>
2025-08-25 18:17:05 +0900

    Refactor : 모델 변경 callerNavigation 인자 콜백 제거 홈에서만 필요했었음

--- app/src/main/java/com/likelion/siso/navigation/MainNavHost.kt
+++ app/src/main/java/com/likelion/siso/navigation/MainNavHost.kt
@@ -24,7 +24,7 @@ fun MainNavHost(
     modifier: Modifier = Modifier,
     appState: SisoAppState,
     //startDestination: String = NavigationRoute.OnBoardingScreen.route
-    startDestination: String = NavigationRoute.LoginScreen.route
+    startDestination: String = NavigationRoute.HomeScreen.route
 ) {
     NavHost(
         modifier = modifier,
@@ -68,7 +68,7 @@ fun MainNavHost(
                         launchSingleTop = true
                     }
                 )
-            }
+            },
         ){
             appState.navController.navigateToHome()
         }
@@ -90,16 +90,9 @@ fun MainNavHost(
             appState.navController.navigateToMyPage()
         }
         callerNavigation(
-            action = { }, // 예시로 스낵바 동작 추가
-            // onNavigateToCallForCaller 람다 정의
-            onNavigateToCallForCaller = { userId, otherUserId ->
-                appState.navController.navigateToCallForCaller(
-                    userId = userId,
-                    otherUserId = otherUserId,
-                    navOptions = navOptions {
-                        launchSingleTop = true
-                    }
-                )
+            action = { },
+            onNavigateUp = {
+                appState.navController.popBackStack()
             }
         )
 
