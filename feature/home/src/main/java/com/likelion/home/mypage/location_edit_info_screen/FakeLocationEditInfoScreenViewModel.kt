ebdccdb336de8a87a515842c1e16ef3108f798e0
jaeminKim <kg8480@gmail.com>
2025-08-26 02:18:06 +0900

    유즈케이스 적용 하여 수정한 뷰모델

--- feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/FakeLocationEditInfoScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/FakeLocationEditInfoScreenViewModel.kt
@@ -1,11 +1,13 @@
 package com.likelion.home.mypage.location_edit_info_screen
 
 import androidx.lifecycle.ViewModel
+import com.likelion.domain.mypage.model.Location
 import com.likelion.domain.mypage.usecase.BottomLocationUseCase
 import com.likelion.domain.mypage.usecase.TopLocationUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import timber.log.Timber.Forest.d
 import javax.inject.Inject
 
 class FakeLocationEditInfoScreenViewModel (
@@ -14,12 +16,29 @@ class FakeLocationEditInfoScreenViewModel (
     private val bottomLocationUseCase: BottomLocationUseCase
 ): LocationEditInfoScreenViewModelType {
     private val _locationState = MutableStateFlow("")
+
     private val _topLocation = MutableStateFlow(topLocationUseCase.invoke())
-    val topLocation = _topLocation.asStateFlow()
-    override fun locationComplete(input: String, nav :()-> Unit){
+    override val topLocation = _topLocation.asStateFlow()
+    private val _bottomLocation = MutableStateFlow(Location(name = listOf()))
+    override val bottomLocation = _bottomLocation.asStateFlow()
+    
+    init {
+        _topLocation.update {
+            d("topLocationUseCase.invoke() : ${topLocationUseCase.invoke()}")
+            topLocationUseCase.invoke() }
+    }
+
+    override fun setBottomLocation(input: String){
+        val inputBottom = if(input != "")
+            bottomLocationUseCase.invoke(input)
+        else Location(name = listOf())
+        _bottomLocation.update { inputBottom }
+    }
+
+    override fun locationComplete(input: String, nav: (String) -> Unit) {
         _locationState.update { input }
         if (_locationState.value.isNotBlank()){
-            nav()
+            nav(_locationState.value)
         }
     }
 }
