ec661850c6aef3ba69a327c49412a95bc48a3567
jaeminKim <kg8480@gmail.com>
2025-08-26 02:19:52 +0900

    json 적용한 repo

--- data/src/main/java/com/likelion/data/mypage/repository/LocationRepositoryImpl.kt
+++ data/src/main/java/com/likelion/data/mypage/repository/LocationRepositoryImpl.kt
@@ -1,23 +1,32 @@
 package com.likelion.data.mypage.repository
 
+import android.util.Log.d
 import com.google.gson.Gson
-import com.likelion.data.mypage.mapper.LocationMapper
+import com.likelion.data.mypage.mapper.toBottomDomain
+import com.likelion.data.mypage.mapper.toTopDomain
 import com.likelion.data.mypage.model.LocationEntity
 import com.likelion.domain.mypage.model.Location
+import com.likelion.domain.mypage.repository.FakeLocationEntity
 import com.likelion.domain.mypage.repository.LocationRepository
+import com.likelion.domain.mypage.repository.toBottomDomain
+import com.likelion.domain.mypage.repository.toTopDomain
 import javax.inject.Inject
 
 class LocationRepositoryImpl @Inject constructor(
-    private val locationMapper: LocationMapper
-): LocationRepository {
-    private val locationList =
-        Gson().fromJson("korea_regions_ordered.json", LocationEntity::class.java)
 
-    override fun getTopLocationList(): Location {
-        return locationMapper.mapEntityToTopLocation(locationList)
+): LocationRepository {
+
+    private var locationList = LocationEntity(listOf())
+
+    override fun setJson(json: String) {
+        locationList = Gson().fromJson(json, LocationEntity::class.java)
     }
 
-    override fun getBottomLocationList(topName: String): List<Location> {
-        return locationMapper.mapEntityToBottomLocation(locationList, topName)
+    override fun getTopLocationList(): Location {
+        return locationList.toTopDomain()
+    }
+
+    override fun getBottomLocationList(topName: String): Location {
+        return locationList.toBottomDomain(topName)
     }
 }
