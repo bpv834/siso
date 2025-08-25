30edc8a8070bdc10ab00c968cc1f12e1045129b2
jaeminKim <kg8480@gmail.com>
2025-08-26 02:18:41 +0900

    data 코드 정리

--- data/src/main/java/com/likelion/data/mypage/model/LocationEntity.kt
+++ data/src/main/java/com/likelion/data/mypage/model/LocationEntity.kt
@@ -1,14 +1,10 @@
 package com.likelion.data.mypage.model
 
 data class LocationEntity(
-    val locationList: List<LocationListEntity>, // List<String>
-) {
-
-}
+    val locationList: List<LocationListEntity>
+)
 
 data class LocationListEntity(
-    val topName: String, // VARCHAR(255), NOT NULL
-    val bottomName: List<String>, // VARCHAR(255), NOT NULL
-) {
-
-}
+    val bottomName: List<String>,
+    val topName: String
+)
