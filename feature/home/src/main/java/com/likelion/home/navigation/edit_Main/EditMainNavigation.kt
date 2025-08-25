66ce926ea872e9ac6782724748becccd63ff49d5
jaeminKimPea <kg8480@gmail.com>
2025-08-25 16:50:48 +0900

    위치 수정 네비게이션 수정

--- feature/home/src/main/java/com/likelion/home/navigation/edit_Main/EditMainNavigation.kt
+++ feature/home/src/main/java/com/likelion/home/navigation/edit_Main/EditMainNavigation.kt
@@ -5,20 +5,27 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.google.android.material.snackbar.Snackbar
+import com.likelion.domain.mypage.usecase.BottomLocationUseCase
+import com.likelion.domain.mypage.usecase.TopLocationUseCase
 import com.likelion.home.mypage.MyPageRoute
 import com.likelion.navigation.NavigationRoute
+import java.util.Objects
 
 fun NavController.navigateToEditMain(navOptions: NavOptions? = null) =
     navigate(NavigationRoute.MyPageScreen.MainEditScreen.route, navOptions)
 
 fun NavGraphBuilder.editMainNavigation(
     navController: NavController,
+    topLocationUseCase : TopLocationUseCase,
+    bottomLocationUseCase : BottomLocationUseCase,
     actionSnackbar : () -> Unit
 ) {
     composable(
         route = NavigationRoute.MyPageScreen.MainEditScreen.route
     ) {
         EditMainRoute (
+            topLocationUseCase = topLocationUseCase,
+            bottomLocationUseCase = bottomLocationUseCase,
             actionSnackbar = actionSnackbar
         )
     }
