0ff19ab4580324c3ef90db9a5ff9218bd5371cd5
jaeminKim <kg8480@gmail.com>
2025-08-26 02:19:11 +0900

    LocationMapper.kt 코드 오류 수정

--- data/src/main/java/com/likelion/data/mypage/mapper/LocationMapper.kt
+++ data/src/main/java/com/likelion/data/mypage/mapper/LocationMapper.kt
@@ -12,26 +12,20 @@ fun LocationEntity.toTopDomain(): Location{
 }
 
 fun LocationEntity.toBottomDomain(
-    entity: LocationEntity,
     topName: String
-): List<Location>{
+): Location{
     // 변환 하위 -> 상위
-    locationList
-    val locationList = mutableListOf<Location>()
-    entity.locationList.forEach {
-        if (it.topName == topName) {
-            locationList.add(Location(it.bottomName))
-            return@forEach
-        }
-    }
-    return locationList
+    return Location(
+        name = locationList.filter {
+        it.topName == topName
+    }.map {
+        it.bottomName
+    }.first()
+    )
 }
 
+// 상위 -> 하위
+fun Location.mapDomainToEntity(domain: String,bottomName: List<String>){
+    //
 
-class LocationMapper {
-    // 상위 -> 하위
-    fun mapDomainToEntity(domain: String,bottomName: List<String>){
-        //
-
-    }
 }
