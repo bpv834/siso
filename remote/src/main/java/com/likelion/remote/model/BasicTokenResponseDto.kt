983c9b28a398ecb78d572e5f8555aa3e29462b72
zzangse1 <zzangse1014@gmail.com>
2025-08-26 17:07:01 +0900

    Feat: api명세서

---
+++ remote/src/main/java/com/likelion/remote/model/BasicTokenResponseDto.kt
@@ -0,0 +1,12 @@
+package com.likelion.remote.model
+
+import com.squareup.moshi.JsonClass
+
+
+@JsonClass(generateAdapter = true)
+data class BasicTokenResponseDto(
+    val refreshToken: String,
+    val registrationStatus: RegistrationStatus
+)
+
+enum class RegistrationStatus { REGISTER, LOGIN }
