89379bd2ea46797175109e55886cdca094b3b185
zzangse1 <zzangse1014@gmail.com>
2025-08-26 17:09:30 +0900

    Feat: 서버 연동 구현체

--- data/src/main/java/com/likelion/data/login/repository/LoginRepositoryImpl.kt
+++ data/src/main/java/com/likelion/data/login/repository/LoginRepositoryImpl.kt
@@ -1,28 +1,43 @@
 package com.likelion.data.login.repository
 
-import com.likelion.domain.login.model.TokenPair
+import com.likelion.data.login.mapper.toDomain
+import com.likelion.domain.login.model.BasicToken
+import com.likelion.domain.login.model.PostKakaoResult
+import com.likelion.domain.login.model.User
 import com.likelion.domain.login.repository.LoginRepository
 import com.likelion.remote.api.KakaoAuthApiService
-import com.likelion.remote.model.request.KakaoAuthRequest
-import kotlinx.coroutines.coroutineScope
+import com.likelion.remote.model.KakaoAccessTokenRequestDto
 import javax.inject.Inject
 
 class LoginRepositoryImpl @Inject constructor(
     private val kakaoApi: KakaoAuthApiService
 ) : LoginRepository {
-    override suspend fun exchangeKakaoToken(
-        kakaoAccessToken: String,
-        codeVerifier: String
-    ): TokenPair {
-        val dto = kakaoApi.loginWithKakao(
-            KakaoAuthRequest(
-                accessToken = kakaoAccessToken,
-                codeVerifier = codeVerifier
-            )
-        )
-        return TokenPair(
-            accessToken = dto.accessToken,
-            refreshToken = dto.refreshToken
-        )
+    // 서버에 카카오 액세스 토큰을 보냄
+    override suspend fun postKakaoAccessToken(token: String): PostKakaoResult {
+        return try {
+            val response = kakaoApi.postKakaoToken(KakaoAccessTokenRequestDto(accessToken = token))
+            if (response.isSuccessful) {
+                val body = response.body() ?: return PostKakaoResult.Error(code = 500, message = "Empty body")
+                PostKakaoResult.Success(
+                    body.toDomain()
+                )
+            } else {
+                PostKakaoResult.Error(response.code(), response.message())
+            }
+
+        } catch (t: Throwable) {
+            PostKakaoResult.Exception(t)
+        }
+    }
+
+    // 서버에 리프래시 토큰 재발급
+    override suspend fun postRefreshToken(token: BasicToken): User {
+        val response = kakaoApi.refreshServerToken("Bearer ${token.refreshToken}")
+        if (response.isSuccessful) {
+            val body = response.body() ?: throw IllegalStateException("Empty refresh body")
+            return body.toDomain()
+        } else {
+            throw IllegalStateException("HTTP ${response.code()} ${response.message()}")
+        }
     }
 }
