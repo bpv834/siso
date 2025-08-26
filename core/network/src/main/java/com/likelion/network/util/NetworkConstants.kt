36d5e3ea8ded4a27250ab9a8f71b97a1bdf23c18
zzangse1 <zzangse1014@gmail.com>
2025-08-26 17:04:08 +0900

    Feat: Base_URL 설정

--- core/network/src/main/java/com/likelion/network/util/NetworkConstants.kt
+++ core/network/src/main/java/com/likelion/network/util/NetworkConstants.kt
@@ -1,5 +1,8 @@
 package com.likelion.network.util
 
 object NetworkConstants {
-    const val BASE_URL = "https://c0c1b219a4a5.ngrok-free.app/"
+  //  const val BASE_URL = "https://c0c1b219a4a5.ngrok-free.app/"
+    //const val BASE_URL = "https://5137505906b1.ngrok-free.app/"
+    const val BASE_URL = "http://13.124.11.3:8080/"
+
 }
