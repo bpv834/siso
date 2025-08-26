a086fff427af87ff92731ce58a70fafca6a2526f
jaeminKim <kg8480@gmail.com>
2025-08-27 00:53:11 +0900

    뷰모델 과 코드 일치

--- feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/FakeLocationEditInfoScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/FakeLocationEditInfoScreenViewModel.kt
@@ -1,10 +1,14 @@
 package com.likelion.home.mypage.location_edit_info_screen
 
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.likelion.domain.mypage.LocationState
 import com.likelion.domain.mypage.model.Location
 import com.likelion.domain.mypage.usecase.BottomLocationUseCase
 import com.likelion.domain.mypage.usecase.TopLocationUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import timber.log.Timber.Forest.d
@@ -13,15 +17,19 @@ import javax.inject.Inject
 class FakeLocationEditInfoScreenViewModel (
     // usecase자리
     private val topLocationUseCase: TopLocationUseCase,
-    private val bottomLocationUseCase: BottomLocationUseCase
+    private val bottomLocationUseCase: BottomLocationUseCase,
 ): LocationEditInfoScreenViewModelType {
-    private val _locationState = MutableStateFlow("")
+    private val _completeState = MutableStateFlow("")
 
     private val _topLocation = MutableStateFlow(topLocationUseCase.invoke())
     override val topLocation = _topLocation.asStateFlow()
     private val _bottomLocation = MutableStateFlow(Location(name = listOf()))
     override val bottomLocation = _bottomLocation.asStateFlow()
-    
+    private val _locationState = MutableStateFlow<LocationState>(LocationState.Loading)
+    override val locationState: StateFlow<LocationState> = _locationState
+    private var _currentLocation = mutableStateOf("")
+    override val currentLocation = _currentLocation.value
+
     init {
         _topLocation.update {
             d("topLocationUseCase.invoke() : ${topLocationUseCase.invoke()}")
@@ -36,9 +44,12 @@ class FakeLocationEditInfoScreenViewModel (
     }
 
     override fun locationComplete(input: String, nav: (String) -> Unit) {
-        _locationState.update { input }
-        if (_locationState.value.isNotBlank()){
-            nav(_locationState.value)
+        _completeState.update { input }
+        if (_completeState.value.isNotBlank()){
+            nav(_completeState.value)
         }
+    }
+    override fun fetchUserLocation() {
+
     }
 }
