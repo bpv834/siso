d03b032267618f12847c76bc0b2f859772cae65b
zzangse1 <zzangse1014@gmail.com>
2025-08-26 17:04:26 +0900

    Feat: UI상태 업데이트

--- feature/login/src/main/java/com/likelion/login/state/LoginUiState.kt
+++ feature/login/src/main/java/com/likelion/login/state/LoginUiState.kt
@@ -1,7 +1,11 @@
 package com.likelion.login.state
 
+import com.likelion.domain.login.model.UserStatus
+
+// 자동로그인 시도
 data class LoginUiState(
-    val isLoggedIn: Boolean = false,
-    val kakaoToken: String? = null,
+    val accessToken: String? = null,
+    val refreshToken: String? = null,
+    val userState: UserStatus = UserStatus.NONE,
     val error: String? = null
 )
