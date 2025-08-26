ce83dd5419f7dbbb7d85a023b99b9a43da812c43
jaeminKim <kg8480@gmail.com>
2025-08-27 00:54:51 +0900

    api usecase 에 쓸 변수 삽입

--- feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/LocationEditInfoScreenViewModelType.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/location_edit_info_screen/LocationEditInfoScreenViewModelType.kt
@@ -1,5 +1,7 @@
 package com.likelion.home.mypage.location_edit_info_screen
 
+import androidx.compose.runtime.mutableIntStateOf
+import com.likelion.domain.mypage.LocationState
 import com.likelion.domain.mypage.model.Location
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -8,6 +10,9 @@ import kotlinx.coroutines.flow.asStateFlow
 interface LocationEditInfoScreenViewModelType {
     val topLocation: StateFlow<Location>
     val bottomLocation:StateFlow<Location>
+    val currentLocation: String
+    val locationState: StateFlow<LocationState>
     fun setBottomLocation(input: String)
     fun locationComplete(input: String, nav :(String)-> Unit)
+    fun fetchUserLocation()
 }
