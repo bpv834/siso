842916da28960d23b5ddd5cef81f001db8a74772
jaeminKim <kg8480@gmail.com>
2025-08-27 00:56:30 +0900

    api usecase repo 모듈 삽입

--- data/src/main/java/com/likelion/data/mypage/di/MyPageModule.kt
+++ data/src/main/java/com/likelion/data/mypage/di/MyPageModule.kt
@@ -1,10 +1,17 @@
 package com.likelion.data.mypage.di
 
+import android.content.Context
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.likelion.data.mypage.repository.APILocationRepositoryImpl
 import com.likelion.data.mypage.repository.LocationRepositoryImpl
+import com.likelion.domain.mypage.repository.APILocationRepository
 import com.likelion.domain.mypage.repository.LocationRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -25,5 +32,9 @@ abstract class MyPageModule {
     @Binds
     @Singleton
     abstract fun bindLocationRepo(impl: LocationRepositoryImpl): LocationRepository
+
+    @Binds
+    @Singleton
+    abstract fun provideLocationRepository(impl: APILocationRepositoryImpl): APILocationRepository
 
 }
