1f6bc18160e9c8b305eddd0f3d63ee3f5a3e63d8
bpv834 <yungjoon2@naver.com>
2025-08-26 21:51:37 +0900

    Feat : 전화 기능 dto, 레포, api 추가

--- remote/src/main/java/com/likelion/remote/model/response/CallInfoDto.kt
+++ remote/src/main/java/com/likelion/remote/model/response/CallInfoDto.kt
@@ -1,8 +1,13 @@
 package com.likelion.remote.model.response
 
+import com.google.gson.annotations.SerializedName
+
 // 서버로부터 받을 통화 정보 데이터 클래스
 data class CallInfoDto(
-    val channelName: String,
-    val token: String,
-    val uid: Int = 0 // 서버에서 사용자 ID를 할당할 경우를 대비
+    @SerializedName("id") val id: Long, // 서버 DTO에 id 필드가 있으므로 추가
+    @SerializedName("channelName") val channelName: String,
+    @SerializedName("token") val token: String,
+    @SerializedName("callerId") val callerId: Long, // 서버 DTO에 callerId 필드가 있으므로 추가
+    @SerializedName("receiverId") val receiverId: Long // 서버 DTO에 receiverId 필드가 있으므로 추가
+    // TODO: 서버 DTO에 uid 필드는 없으므로 제거했습니다. 필요하다면 서버와 협의 후 추가해야 합니다.
 )
