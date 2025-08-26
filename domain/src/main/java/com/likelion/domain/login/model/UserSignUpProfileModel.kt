7e74ec82a3e97059aa82c784792cb6821d7ea5e6
bpv834 <yungjoon2@naver.com>
2025-08-26 18:09:45 +0900

    Feat : 유저프로필 모델 생성

--- domain/src/main/java/com/likelion/domain/login/model/UserSignUpProfileModel.kt
+++ domain/src/main/java/com/likelion/domain/login/model/UserSignUpProfileModel.kt
@@ -1,11 +1,11 @@
 package com.likelion.domain.login.model
 
 data class UserSignUpProfile(
-    val nickname: String = "",
-    val age: Int = 0,
-    val gender: String = "",
-    val preferenceSex: String ="상관없음",
-    val photoPaths: List<String> = emptyList(), // 사진 파일 경로 목록
-    val introduce: String = "",
-    val voicePath: String = "" // 녹음 파일 경로
+    var nickname: String = "",
+    var age: Int = 0,
+    var gender: String = "",
+    var preferenceSex: String ="상관없음",
+    var photoPaths: List<String> = emptyList(), // 사진 파일 경로 목록
+    var introduce: String = "",
+    var voicePath: String = "" // 녹음 파일 경로
 )
