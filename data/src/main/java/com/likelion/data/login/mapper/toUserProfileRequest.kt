a8dfc3cb5a06c0abe73a1ac1fae3b39e647da176
bpv834 <yungjoon2@naver.com>
2025-08-27 20:05:51 +0900

    Fix : 이넘값으로 변경

--- data/src/main/java/com/likelion/data/login/mapper/toUserProfileRequest.kt
+++ data/src/main/java/com/likelion/data/login/mapper/toUserProfileRequest.kt
@@ -7,12 +7,13 @@ import com.likelion.data.enum_pack.PreferenceContact
 import com.likelion.data.enum_pack.Religion
 import com.likelion.domain.login.model.UserSignUpProfile
 import com.likelion.remote.model.request.UserProfileRequest
+import timber.log.Timber
 
 
 // UserSignUpProfile을 UserProfileRequest로 변환하는 확장 함수
 fun UserSignUpProfile.toUserProfileRequest(): UserProfileRequest {
-    return UserProfileRequest(
-        drinkingCapacity = "Naver", // TODO: 실제 enum 값 또는 매핑 로직 필요
+    val result =UserProfileRequest(
+        drinkingCapacity = DrinkingCapacity.FREQUENTLY.name, // TODO: 실제 enum 값 또는 매핑 로직 필요
         religion = Religion.CHRISTIANITY.name, // TODO: 실제 enum 값 또는 매핑 로직 필요
         smoke = false, // TODO: 실제 값 매핑 필요
         age = this.age,
@@ -23,6 +24,8 @@ fun UserSignUpProfile.toUserProfileRequest(): UserProfileRequest {
         sex = this.gender, // 클라이언트의 gender를 서버의 sex로 매핑
         preferenceSex = this.preferenceSex
     )
+    Timber.d("result = $result")
+    return result
 }
 
 // TODO: UserProfileResponse를 다시 클라이언트의 UserProfile 모델로 변환하는 매퍼도 필요할 수 있습니다.
