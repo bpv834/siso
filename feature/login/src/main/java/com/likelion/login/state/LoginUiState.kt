2ea4763d906ac553421d2c1819880d0595b3852f
zzangse1 <zzangse1014@gmail.com>
2025-08-27 11:12:34 +0900

    Feat: UiState user 추가

--- feature/login/src/main/java/com/likelion/login/state/LoginUiState.kt
+++ feature/login/src/main/java/com/likelion/login/state/LoginUiState.kt
@@ -1,5 +1,6 @@
 package com.likelion.login.state
 
+import com.likelion.domain.login.model.User
 import com.likelion.domain.login.model.UserStatus
 
 // 자동로그인 시도
@@ -7,5 +8,6 @@ data class LoginUiState(
     val accessToken: String? = null,
     val refreshToken: String? = null,
     val userState: UserStatus = UserStatus.NONE,
-    val error: String? = null
+    val user: User? = null,
+    val error: String? = null,
 )
