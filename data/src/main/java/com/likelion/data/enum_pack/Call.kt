dffd7de536b3f3cac78785d183eda6eccb35fbc0
bpv834 <yungjoon2@naver.com>
2025-08-27 19:53:47 +0900

    Feat : enum class 추가

---
+++ data/src/main/java/com/likelion/data/enum_pack/Call.kt
@@ -0,0 +1,19 @@
+package com.likelion.data.enum_pack
+
+enum class Call(val code: Int, val description: String) {
+    REQUESTED(0, "통화 요청"),
+    ACCEPTED(1, "승낙"),
+    DENIED(2, "거절"),
+    ENDED(3, "통화 종료");
+
+    companion object {
+        fun fromCode(code: Int): Call? {
+            return entries.find { it.code == code }
+        }
+
+        // code -> 서버 전송용 value(Enum name) 뽑기
+        fun getDtoValueFromCode(selectedCode: Int): String? {
+            return fromCode(selectedCode)?.name  // ex) "REQUESTED", "ACCEPTED" ...
+        }
+    }
+}
---
+++ data/src/main/java/com/likelion/data/enum_pack/Report.kt
@@ -0,0 +1,22 @@
+package com.likelion.data.enum_pack
+
+enum class Report(val code: Int, val description: String) {
+    SPAM(0, "스팸성/글/메시지"),
+    INAPPROPRIATE(1, "부적절한 내용(욕설,선정적,폭력적)"),
+    HARASSMENT(2, "괴롭힘,혐오 발언"),
+    IMPERSONATION(3, "사칭,도용"),
+    ILLEGAL_CONTENT(4, "불법 컨텐츠,마약,불법광고"),
+    SEXUAL_CONTENT(5, "성적,음란한 콘텐츠"),
+    VIOLENCE(6, "폭력적/위험적 콘텐츠"),
+    PRIVACY(7, "개인정보 유출"),
+    OTHER(8, "기타(직접 입력)");
+
+    companion object {
+        private val codeMap = values().associateBy { it.code }
+
+        fun fromCode(code: Int): Report? = codeMap[code]
+
+        // code -> 서버 전송용 Enum name 반환
+        fun getDtoValueFromCode(code: Int): String? = fromCode(code)?.name
+    }
+}
