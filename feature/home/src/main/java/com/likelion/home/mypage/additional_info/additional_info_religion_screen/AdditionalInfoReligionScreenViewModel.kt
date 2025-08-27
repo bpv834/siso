d4d08549f21f71f8e388f3b890632eb37de3e3f3
jaeminKimPea <kg8480@gmail.com>
2025-08-27 13:13:06 +0900

    이전화면에 보내는 클래스 타입 삽입한 상태

--- feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_religion_screen/AdditionalInfoReligionScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_religion_screen/AdditionalInfoReligionScreenViewModel.kt
@@ -1,6 +1,7 @@
 package com.likelion.home.mypage.additional_info.additional_info_religion_screen
 
 import androidx.lifecycle.ViewModel
+import com.likelion.home.navigation.Pub
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -20,6 +21,12 @@ class AdditionalInfoReligionScreenViewModel@Inject constructor (
     }
     override val receiverList = _receiverList.asStateFlow()
 
-
+    override fun updatePubList(list : List<String>,nav:(List<Pub>)->Unit){
+        if (list.size>3 && list.size<7){
+            nav(list.map { Pub(it) })
+        }else{
+            nav(receiverList.value.map { Pub(it) })
+        }
+    }
 
 }
