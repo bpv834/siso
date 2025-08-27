1ecc345121c2612b20456c2f109c3be15555c679
jaeminKimPea <kg8480@gmail.com>
2025-08-27 13:13:11 +0900

    이전화면에 보내는 클래스 타입 삽입한 상태

--- feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_religion_screen/AdditionalInfoReligionScreenViewModelType.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/additional_info/additional_info_religion_screen/AdditionalInfoReligionScreenViewModelType.kt
@@ -1,9 +1,11 @@
 package com.likelion.home.mypage.additional_info.additional_info_religion_screen
 
+import com.likelion.home.navigation.Pub
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 interface AdditionalInfoReligionScreenViewModelType {
     val receiverList : StateFlow<List<String>>
+    fun updatePubList(list : List<String>,nav:(List<Pub>)->Unit)
 }
