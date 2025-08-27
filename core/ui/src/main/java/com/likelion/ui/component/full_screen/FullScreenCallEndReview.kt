1378e2145ae4c8a3821d4f114e617394d86b326c
ho <yungjoon2@naver.com>
2025-08-27 11:13:01 +0900

    Fix : 아이콘 버튼 크기 변경

--- core/ui/src/main/java/com/likelion/ui/component/full_screen/FullScreenCallEndReview.kt
+++ core/ui/src/main/java/com/likelion/ui/component/full_screen/FullScreenCallEndReview.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
@@ -74,7 +76,7 @@ fun FullScreenCallEndReview(
                 color = SisoColorTokens.Gray90
             )
             Spacer(Modifier.size(84.dp))
-            Row {
+            Row (modifier = Modifier.fillMaxWidth().height(150.dp)){
                 CommonOutlinedButtonWithIconVertical(
                     icon = {
                         AsyncImage(
