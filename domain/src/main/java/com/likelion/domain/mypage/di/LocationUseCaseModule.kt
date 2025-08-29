06c886949718d9970901f8ac90a6467306e10901
jaeminKimPea <kg8480@gmail.com>
2025-08-29 13:17:43 +0900

    image 모델 함수 변경

--- domain/src/main/java/com/likelion/domain/mypage/di/LocationUseCaseModule.kt
+++ domain/src/main/java/com/likelion/domain/mypage/di/LocationUseCaseModule.kt
@@ -1,10 +1,13 @@
 package com.likelion.domain.mypage.di
 
+import com.likelion.domain.home.repository.FakeUsersRepositoryImpl
 import com.likelion.domain.mypage.repository.APILocationRepository
 import com.likelion.domain.mypage.repository.LocationRepository
 import com.likelion.domain.mypage.repository.UserFullRepository
+import com.likelion.domain.mypage.repository.UserImageRepository
 import com.likelion.domain.mypage.usecase.BottomLocationUseCase
 import com.likelion.domain.mypage.usecase.CurrentLocationSetUseCase
+import com.likelion.domain.mypage.usecase.GetUserImagesUseCase
 import com.likelion.domain.mypage.usecase.TopLocationUseCase
 import com.likelion.domain.mypage.usecase.UsersFullUseCase
 import dagger.Module
@@ -36,4 +39,10 @@ object LocationUseCaseModule {
     @Singleton
     fun provideUserFullUseCase(userFullRepository: UserFullRepository): UsersFullUseCase =
         UsersFullUseCase(userFullRepository)
+
+    @Provides
+    @Singleton
+    fun provideGetUserImagesUseCase(userFullRepository: UserImageRepository): GetUserImagesUseCase =
+        GetUserImagesUseCase(userFullRepository)
+
 }
