a059b8adc1124a9e52235e9b0b98887506e2574f
bpv834 <yungjoon2@naver.com>
2025-08-30 01:56:31 +0900

    Fix : 서버에 fcm 토큰 보내는 로직 추가

--- data/src/main/java/com/likelion/data/notification/repository/FcmTokenRepositoryImpl.kt
+++ data/src/main/java/com/likelion/data/notification/repository/FcmTokenRepositoryImpl.kt
@@ -3,6 +3,8 @@ package com.likelion.data.notification.repository
 import com.likelion.domain.login.usecase.GetTokenAllUseCase
 import com.likelion.domain.notification.model.FcmToken
 import com.likelion.domain.notification.repository.FcmTokenRepository
+import com.likelion.remote.api.FcmApiService
+import com.likelion.remote.model.request.FcmTokenRequest
 import kotlinx.coroutines.flow.first
 import timber.log.Timber
 import javax.inject.Inject
@@ -10,16 +12,23 @@ import javax.inject.Inject
 
 class FcmTokenRepositoryImpl @Inject constructor(
     // private val api: FcmApiService,
-    private val getTokenUse: GetTokenAllUseCase // 앱에서 JWT 가져오는 헬퍼
+    private val getTokenUse: GetTokenAllUseCase, // 앱에서 JWT 가져오는 usecase
+    // fcm 토큰 서버로 보내는 usecase
+    val fcmApiService: FcmApiService,
 ) : FcmTokenRepository {
     // 클라이언트 sdk 토큰을 따서 서버에 토큰을 매핑하는 메서드
     override suspend fun sendToken(token: FcmToken) {
         // flow라서 first() 로 받음
         val user = getTokenUse.invoke().first()
-        val accessToken = user?.accessToken
+        val refreshToken = user?.refreshToken
 
         Timber.d("user = $user")
-        Timber.d("accessToken= $accessToken")
+        Timber.d("refreshToken= $refreshToken")
+        val request = FcmTokenRequest(
+            userId = user?.userInfo!!.id,
+            token = token.token
+        )
+        fcmApiService.sendToken(jwt = "Bearer $refreshToken", body = request)
     }
 
 }
