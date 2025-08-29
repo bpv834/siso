2a684f6de84234c7121fc3b86286ca51b714b034
bpv834 <yungjoon2@naver.com>
2025-08-30 01:47:47 +0900

    Feat : fcm 토큰 레포지토리 추가

--- data/src/main/java/com/likelion/data/notification/repository/di/DataModule.kt
+++ data/src/main/java/com/likelion/data/notification/repository/di/DataModule.kt
@@ -1,12 +1,6 @@
 package com.likelion.data.notification.repository.di
 
-import com.likelion.data.login.repository.InMemoryUserSignUpRepositoryImpl
-import com.likelion.data.login.repository.LoginRepositoryImpl
-import com.likelion.data.login.repository.TokenRepositoryImpl
 import com.likelion.data.notification.repository.FcmTokenRepositoryImpl
-import com.likelion.domain.login.repository.LoginRepository
-import com.likelion.domain.login.repository.TokenRepository
-import com.likelion.domain.login.repository.UserSignUpRepository
 import com.likelion.domain.notification.repository.FcmTokenRepository
 import dagger.Binds
 import dagger.Module
