41a5c4655bf589d514681dcf5c81f6e980acc1a2
jaeminKimPea <kg8480@gmail.com>
2025-08-29 13:17:43 +0900

    image 모델 함수 변경

--- data/src/main/java/com/likelion/data/mypage/di/MyPageModule.kt
+++ data/src/main/java/com/likelion/data/mypage/di/MyPageModule.kt
@@ -1,7 +1,9 @@
 package com.likelion.data.mypage.di
 
 import com.likelion.data.mypage.repository.UserFullRepositoryImpl
+import com.likelion.data.mypage.repository.UserImageRepositoryImpl
 import com.likelion.domain.mypage.repository.UserFullRepository
+import com.likelion.domain.mypage.repository.UserImageRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,5 +17,9 @@ abstract class MyPageModule {
     @Binds
     @Singleton
     abstract fun bindUserFullRepository(impl: UserFullRepositoryImpl): UserFullRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindUserImageRepository(impl: UserImageRepositoryImpl): UserImageRepository
 
 }
