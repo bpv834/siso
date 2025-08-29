be7252eba0928b09419a20491d409ecfd3e501e6
jaeminKimPea <kg8480@gmail.com>
2025-08-29 13:10:02 +0900

    image 모델 함수 변경

--- feature/home/src/main/java/com/likelion/home/mypage/poto_edit_info_screen/FakePotoEditInfoScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/mypage/poto_edit_info_screen/FakePotoEditInfoScreenViewModel.kt
@@ -38,10 +38,6 @@ class FakePotoEditInfoScreenViewModel(
         _capturedImages.value = _capturedImages.value + newImage
     }
 
-    override fun addAllImageFromAlbum(list: List<Bitmap>) {
-        _capturedImages.value += list
-    }
-
     // 사진 삭제 메서드
     override fun deleteBitMap(delete: Bitmap) {
         val mutableList = _capturedImages.value.toMutableList()
