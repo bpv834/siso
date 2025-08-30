35846137c95e598a4f492434043c8d222b04582e
ho <yungjoon2@naver.com>
2025-08-30 20:00:58 +0900

    Feat : 프로필, 이미지, 보이스 등록

--- domain/src/main/java/com/likelion/domain/image/usecase/UploadImageUseCase.kt
+++ domain/src/main/java/com/likelion/domain/image/usecase/UploadImageUseCase.kt
@@ -6,7 +6,7 @@ import javax.inject.Inject
 class UploadImageUseCase @Inject constructor(
     val repository: ImageRepository
 ) {
-    suspend fun execute() {
-        repository.upLoadImage(refreshToken = "", imgPathList = emptyList())
+    suspend fun execute(refreshToke : String, imgList:List<String>) {
+        repository.upLoadImage(refreshToken = refreshToke, imgPathList = imgList)
     }
 }
