b0ff47f62355a4889d2b8644d7b40768ee95d63f
bpv834 <yungjoon2@naver.com>
2025-08-30 12:43:08 +0900

    Fix : 상속 레포지토리 변경 SignUpRepository -> UserProfileRepository

--- domain/src/main/java/com/likelion/domain/login/usecase/AddProfileUseCase.kt
+++ domain/src/main/java/com/likelion/domain/login/usecase/AddProfileUseCase.kt
@@ -1,12 +1,11 @@
 package com.likelion.domain.login.usecase
 
 import com.likelion.domain.login.model.UserSignUpProfile
-import com.likelion.domain.login.repository.InMemoryUserSignUpRepository
-import com.likelion.domain.login.repository.SignUpRepository
+import com.likelion.domain.login.repository.UserProfileRepository
 import javax.inject.Inject
 
 class AddProfileUseCase @Inject constructor(
-    private val repository : SignUpRepository
+    private val repository : UserProfileRepository
 ) {
     suspend fun execute(refreshToken : String, user : UserSignUpProfile){
         repository.addProfile(refreshToken = refreshToken, profile = user)
