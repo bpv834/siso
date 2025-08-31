98241980c7892a73288cb8df246750f3634445b5
bpv834 <yungjoon2@naver.com>
2025-09-01 00:31:02 +0900

    Refactor : 홈 유저 정보 가져오기

--- feature/home/src/main/java/com/likelion/home/home_page/FakeHomeScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/home_page/FakeHomeScreenViewModel.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+/*
 
 class FakeHomeScreenViewModel(val getAllUsersUseCase: GetAllUsersUseCase) :
     HomeScreenViewModelType {
@@ -53,4 +54,4 @@ class FakeHomeScreenViewModel(val getAllUsersUseCase: GetAllUsersUseCase) :
         TODO("Not yet implemented")
     }
 
-}
+}*/
--- feature/home/src/main/java/com/likelion/home/home_page/HomeScreenViewModelType.kt
+++ feature/home/src/main/java/com/likelion/home/home_page/HomeScreenViewModelType.kt
@@ -4,8 +4,9 @@ import com.likelion.domain.home.model.UsersModel
 import kotlinx.coroutines.flow.StateFlow
 
 interface HomeScreenViewModelType {
-    val userList: StateFlow<List<UsersModel>>
-    fun getUserList()
-    fun onClickCallButton(callerId : Long =0L ,receiverId:Long =1L)
-    fun resetCallState() // 통화 실패 상태를 초기화하는 함수 추가
+    val uiState: StateFlow<HomeScreenUiState>
+
+    fun onEvent(event: HomeScreenUiEvent)
+    fun resetCallState()
+    fun onClickCallButton(receiverId: Long)
 }
