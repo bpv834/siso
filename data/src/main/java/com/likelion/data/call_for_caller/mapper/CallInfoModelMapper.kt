5c3bb91c7145f0dd07f92172aacbab3ddc88dea2
bpv834 <yungjoon2@naver.com>
2025-09-03 23:49:05 +0900

    Feat : 매퍼 추가

--- data/src/main/java/com/likelion/data/call_for_caller/mapper/CallInfoModelMapper.kt
+++ data/src/main/java/com/likelion/data/call_for_caller/mapper/CallInfoModelMapper.kt
@@ -1,12 +1,16 @@
 package com.likelion.data.call_for_caller.mapper
 
 import com.likelion.domain.call_for_caller.model.CallInfoModel
+import com.likelion.domain.call_for_caller.model.CallModel
+import com.likelion.remote.model.request.CallInfoRequest
 import com.likelion.remote.model.response.CallInfoDto
 
-
-fun CallInfoDto.toDomainModel(): CallInfoModel {
-    return CallInfoModel(
+fun CallInfoRequest.toDomain(): CallModel {
+    return CallModel(
+        id = this.id,
+        callerId = this.callerId,
         channelName = this.channelName,
-        token = this.token
+        agoraToken = this.token, // 'token'을 'agoraToken'으로 매핑
+        receiverId = this.receiverId
     )
 }
