3a2475acf4dd3e2db6da6cd829720f747dcbdd70
zzangse1 <zzangse1014@gmail.com>
2025-09-05 02:32:52 +0900

    Feat: 다이얼로그 -> 마이페이지 이동

--- app/src/main/java/com/likelion/siso/navigation/MainNavHost.kt
+++ app/src/main/java/com/likelion/siso/navigation/MainNavHost.kt
@@ -17,6 +17,7 @@ import com.example.notification.FcmEventBus
 import com.likelion.domain.notification.model.Call
 import com.likelion.home.navigation.chatNavigation
 import com.likelion.home.navigation.edit_Main.editMainNavigation
+import com.likelion.home.navigation.edit_Main.navigateToEditMain
 import com.likelion.home.navigation.edit_Main.settingMainNavigation
 import com.likelion.home.navigation.homeNavigation
 import com.likelion.home.navigation.myPageNavigation
@@ -205,6 +206,13 @@ fun MainNavHost(
                     chatRoomId = chatRoomId
                 )
             },
+            onNavigateToEditProfile = {
+                appState.navController.navigateToEditMain(
+                    navOptions {
+                        launchSingleTop = true
+                    }
+                )
+            }
         ) {
             appState.navController.navigateToHome()
         }
