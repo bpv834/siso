259a4eea6122f09fb9b45f9dc328310f77607771
zzangse1 <zzangse1014@gmail.com>
2025-09-05 02:36:11 +0900

    Style: 다이얼로그 상태값 유지

--- feature/home/src/main/java/com/likelion/home/home_page/HomeScreenViewModel.kt
+++ feature/home/src/main/java/com/likelion/home/home_page/HomeScreenViewModel.kt
@@ -3,7 +3,9 @@ package com.likelion.home.home_page
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.likelion.domain.call_for_caller.usecase.StartCallUseCase
+import com.likelion.domain.home.usecase.ChangeDialogStatusUseCase
 import com.likelion.domain.home.usecase.GetAllUsersUseCase
+import com.likelion.domain.home.usecase.GetDialogStatusUseCase
 import com.likelion.domain.login.usecase.GetTokenAllUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -22,11 +24,21 @@ import javax.inject.Inject
 class HomeScreenViewModel @Inject constructor(
     private val getAllUsersUseCase: GetAllUsersUseCase,
     private val getTokenAllUseCase: GetTokenAllUseCase,
+    private val startCallUseCase: StartCallUseCase,
+    private val getDialogStatusUseCase: GetDialogStatusUseCase,
+    private val changeDialogStatusUseCase: ChangeDialogStatusUseCase
 ) : ViewModel() {
 
     // 외부에 노출되는 UI 상태 (데이터)
     private val _uiState = MutableStateFlow<HomeScreenUiState>(HomeScreenUiState.LoadingToken)
     val uiState: StateFlow<HomeScreenUiState> = _uiState.asStateFlow()
+
+    private val _uiStateHomePage = MutableStateFlow(HomePageUiState())
+    val uiStateHomePage: StateFlow<HomePageUiState> = _uiStateHomePage.asStateFlow()
+
+    private val _showHomeDialog = MutableStateFlow(false)
+    val showHomeDialog: StateFlow<Boolean> = _showHomeDialog.asStateFlow()
+
 
     // 외부에 노출되는 부수 효과 (일회성 이벤트)
     private val _sideEffect = MutableSharedFlow<HomeScreenSideEffect>()
@@ -36,39 +48,56 @@ class HomeScreenViewModel @Inject constructor(
 
     init {
         onEvent(HomeScreenUiEvent.GetTokenAndLoadUsers)
+        getDialogStatus()
     }
 
     fun onEvent(event: HomeScreenUiEvent) {
         when (event) {
-            is HomeScreenUiEvent.GetTokenAndLoadUsers -> {
-                Timber.d("getTokenAndLoadUsers")
-                getTokenAndLoadUsers()
-            }
+            is HomeScreenUiEvent.GetTokenAndLoadUsers -> getTokenAndLoadUsers()
+            is HomeScreenUiEvent.OnClickCallButton -> onClickCallButton(
+                receiverId = event.receiverId, accessToken = ""
+            )
 
-            is HomeScreenUiEvent.OnClickCallButton -> {
-                Timber.d("HomeScreenUiEvent.onClickCallButton ")
-                onClickCallButton(
-                    receiverId = event.receiverId,
-                )
+            is HomeScreenUiEvent.ChangeDialogStatus -> changeDialogStatus(event.isDialog)
+            HomeScreenUiEvent.GetDialogStatus -> getDialogStatus()
+
+        }
+    }
+
+    private fun changeDialogStatus(isDialog: Boolean) {
+        _showHomeDialog.value = isDialog
+        _uiStateHomePage.update { it.copy(isDialog = isDialog) }
+
+        viewModelScope.launch {
+            runCatching { changeDialogStatusUseCase(isDialog) }
+                .onFailure { e -> Timber.e(e, "다이얼로그 상태 변경 실패") }
+        }
+    }
+
+    private fun getDialogStatus() {
+        viewModelScope.launch {
+            val skip = try {
+                getDialogStatusUseCase().firstOrNull() ?: false
+            } catch (e: Exception) {
+                Timber.e("Error: ${e.message}")
+                false
             }
+            _uiStateHomePage.update { it.copy(isDialog = skip) }
+            Timber.d("다이얼로그 상태: $skip")
         }
     }
 
     private fun getTokenAndLoadUsers() {
         viewModelScope.launch {
-            Timber.d("🔵 [getTokenAndLoadUsers] 토큰 로딩 시작")
             _uiState.value = HomeScreenUiState.LoadingToken
 
-            getTokenAllUseCase.invoke().collect { tokenInfo ->
-                val userToken = tokenInfo?.accessToken
-                Timber.d("🟡 [getTokenAndLoadUsers] 가져온 토큰 = $userToken")
+            val userToken = getTokenAllUseCase.invoke().firstOrNull()?.accessToken
 
-                if (!userToken.isNullOrEmpty()) {
-                    Timber.d("✅ [getTokenAndLoadUsers] 토큰 정상, 유저 불러오기 시작")
-                    accessToken = userToken // 처음 토큰flow에 값을 넣어준다.
-                    loadUsers(userToken)
-                    return@collect // 유저 불러온 뒤 종료
-                }
+            if (userToken.isNullOrEmpty()) {
+                _uiState.value = HomeScreenUiState.Error("토큰을 가져오지 못했습니다.")
+            } else {
+                accessToken = userToken
+                loadUsers(userToken)
             }
         }
     }
@@ -78,7 +107,6 @@ class HomeScreenViewModel @Inject constructor(
             _uiState.value = HomeScreenUiState.LoadingUsers
 
             val result = getAllUsersUseCase.execute(token)
-            Timber.d("result = ${result}")
 
             if (result.isSuccess) {
                 _uiState.value = HomeScreenUiState.Success(result.getOrThrow())
@@ -90,16 +118,19 @@ class HomeScreenViewModel @Inject constructor(
         }
     }
 
-    private fun onClickCallButton(receiverId: Long) {
-        Timber.d("onClickCallButton receiverId : $receiverId / accessToken : $accessToken")
+    private fun onClickCallButton(receiverId: Long, accessToken: String) {
         viewModelScope.launch {
-            try {
-               // startCallUseCase.execute(receiverId = receiverId, accessToken = accessToken!!)
-                _sideEffect.emit(HomeScreenSideEffect.NavigateToCaller(receiverId))
-            } catch (e: Exception) {
-                _sideEffect.emit(HomeScreenSideEffect.ShowSnackbar("통화 실패: ${e.message}"))
+            val token = accessToken
+            if (token != null) {
+                try {
+                    startCallUseCase.execute(receiverId = receiverId, accessToken = accessToken)
+                    _sideEffect.emit(HomeScreenSideEffect.NavigateToCaller(receiverId))
+                } catch (e: Exception) {
+                    _sideEffect.emit(HomeScreenSideEffect.ShowSnackbar("통화 실패: ${e.message}"))
+                }
+            } else {
+                _sideEffect.emit(HomeScreenSideEffect.ShowSnackbar("유효한 토큰이 없어 통화를 시작할 수 없습니다."))
             }
         }
     }
-
 }
