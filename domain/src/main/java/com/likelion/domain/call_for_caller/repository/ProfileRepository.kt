3bce8096522bf83f148040177a3abb0adbbef0b0
bpv834 <yungjoon2@naver.com>
2025-09-05 00:20:34 +0900

    Fix : 인자 추가 accessToken

--- domain/src/main/java/com/likelion/domain/call_for_caller/repository/ProfileRepository.kt
+++ domain/src/main/java/com/likelion/domain/call_for_caller/repository/ProfileRepository.kt
@@ -3,5 +3,5 @@ package com.likelion.domain.call_for_caller.repository
 import com.likelion.domain.call_for_caller.model.UserProfileModel
 
 interface ProfileRepository {
-    suspend fun getUserProfile(userId: Long): Result<UserProfileModel>
+    suspend fun getUserProfile(accessToken: String, userId: Long): Result<UserProfileModel>
 }
