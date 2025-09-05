9c6cdc7fc0be2a00145f75439119639293784d95
ho <yungjoon2@naver.com>
2025-09-05 12:17:00 +0900

    Refactor : 인자변경

--- feature/home/src/main/java/com/likelion/home/home_page/HomeScreenUiEvent.kt
+++ feature/home/src/main/java/com/likelion/home/home_page/HomeScreenUiEvent.kt
@@ -3,7 +3,7 @@ package com.likelion.home.home_page
 // 사용자 액션 및 일회성 이벤트를 위한 클래스
 sealed class HomeScreenUiEvent {
     object GetTokenAndLoadUsers : HomeScreenUiEvent()
-    data class OnClickCallButton(val callerId: Long, val receiverId: Long) : HomeScreenUiEvent()
+    data class OnClickCallButton(val receiverId: Long) : HomeScreenUiEvent()
     data class ChangeDialogStatus(val isDialog: Boolean) : HomeScreenUiEvent()
     object GetDialogStatus : HomeScreenUiEvent()
 }
