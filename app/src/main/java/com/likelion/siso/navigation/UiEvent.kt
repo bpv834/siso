858997dfd6fbe2653e1c4ed8c9f7bc66e234f897
bpv834 <yungjoon2@naver.com>
2025-09-11 00:27:37 +0900

    Fix : 이벤트 변경 call -> receiverScreen

--- app/src/main/java/com/likelion/siso/navigation/UiEvent.kt
+++ app/src/main/java/com/likelion/siso/navigation/UiEvent.kt
@@ -1,6 +1,5 @@
 package com.likelion.siso.navigation
 
-import com.likelion.domain.call_for_caller.model.CallModel
 import com.likelion.domain.notification.model.Call
 
 sealed class UiEvent {
@@ -8,5 +7,5 @@ sealed class UiEvent {
     object CallReject : UiEvent() // 상대방이 거절
     object CallRejectedByMe: UiEvent() // 내가 거절
     data class Error(val message: String) : UiEvent()
-    data class NavigateToCallScreen (val call : Call): UiEvent() // 수신자가 전화 받았을 때 이동 이벤트
+    data class NavigateToReceiverScreen (val call : Call): UiEvent() // 수신자가 전화 받았을 때 이동 이벤트
 }
