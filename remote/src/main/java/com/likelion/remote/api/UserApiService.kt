ff1dd82c12a33b267ad2cc76623961d2a18be421
bpv834 <yungjoon2@naver.com>
2025-09-11 00:26:36 +0900

    Fix : post -> patch 수정

--- remote/src/main/java/com/likelion/remote/api/UserApiService.kt
+++ remote/src/main/java/com/likelion/remote/api/UserApiService.kt
@@ -1,5 +1,6 @@
 package com.likelion.remote.api
 
+import com.likelion.remote.model.request.NotificationAllowRequest
 import com.likelion.remote.model.request.UserProfileRequest
 import com.likelion.remote.model.response.UserInfo
 import com.likelion.remote.model.response.UserProfileResponse
@@ -7,6 +8,7 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -42,10 +44,10 @@ interface UserApiService {
 
     // POST /api/profiles
     // 사용자 프로필 정보를 등록합니다.
-    @POST("api/users/notification")
+    @PATCH("api/users/notification")
     suspend fun updateNotification(
         @Header("Authorization") accessToken: String, // AccessToken을 사용하도록 변경하는 것이 더 일반적입니다.
-        @Body request: NotificationRequest
+        @Body request: NotificationAllowRequest
     ): Response<Unit> // 반환 값이 없으므로 Unit을 사용합니다.
 
 
